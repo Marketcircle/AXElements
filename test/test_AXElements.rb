e1127b2fc4b5ce09de9aec99157cebe881d52fa7
Mark Rada <mrada@marketcircle.com>
2010-12-11 00:28:20 -0500

    Switch to using minitest/spec

--- test/test_AXElements.rb
+++ test/test_AXElements.rb
@@ -1,7 +1,1 @@
 require 'helper'
-
-class TestAxelements < MiniTest::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
-end
