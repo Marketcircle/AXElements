a1437017f56c161ab4333fdb4fa0070990d549c5
Mark Rada <mrada@marketcircle.com>
2011-01-18 15:18:42 -0500

    Add some checks to make sure dependancies are available
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- lib/AXElements.rb
+++ lib/AXElements.rb
@@ -1,4 +1,18 @@
 framework 'Cocoa'
+
+begin
+  # check that the new bridge support exists
+  # check that the Accessibility APIs are enabled
+  unless AXAPIEnabled()
+    NSLog('The Accessibility APIs are disabled on this machine.')
+    NSLog('Please enable the Accessibility APIs from the System Preferences')
+    exit 3
+  end
+rescue NoMethodError
+  NSLog('You need to install the latest BridgeSupport preview for CoreFoundation access')
+  exit 4
+end
+
 require   'AXElements/monkey'
 require   'AXElements/Traits'
 require   'AXElements/Element'
