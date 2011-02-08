7b5d3905dff48bf975e9c483512ba26ef77cb4d6
Mark Rada <mrada@marketcircle.com>
2011-02-07 20:10:28 -0500

    Make the string extension thread safe
    And also add spec tests to make sure it doesn't break, as it
    turns out to be very easy to break the functionality.
    
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- lib/AXElements/String_ext.rb
+++ lib/AXElements/String_ext.rb
@@ -3,11 +3,16 @@
 # Extensions to the String class.
 class String
 
-  # @todo benchmark against the #split.#capitalize.#join technique
   # Returns the upper camel case version of the string. The string
-  # is assumed to be in snake_case.
+  # is assumed to be in snake_case, but should return an unchanged
+  # string if the string is already in camel case.
+  #
+  # I chose to make this method update the string in place as it
+  # is a fairly hot method and should perform well; by running in
+  # place we save an allocation (which is slow on MacRuby right now).
   # @return [String]
   def camelize!
-    gsub!(/(?:^|_)(.)/) { $1.upcase }
+    gsub! /(?:^|_)(.)/ do |match| match[-1].upcase end
   end
+
 end
