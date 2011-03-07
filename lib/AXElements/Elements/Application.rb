63db856c288de39e460cce29298f5084cd3936f1
Mark Rada <mrada@marketcircle.com>
2011-03-07 16:43:41 -0500

    Update the documentation for AX::Application
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- lib/AXElements/Elements/Application.rb
+++ lib/AXElements/Elements/Application.rb
@@ -1,15 +1,27 @@
 module AX
 
-# Some additional constructors and factories for Application objects.
+##
+# Some additional constructors and conveniences for Application objects.
 class Application < AX::Element
 
   include Traits::Typing
 
   class << self
 
-    # This is the standard way of creating an application object. It will launch
-    # the app if it is not already running.
+    ##
+    # @todo Find a way for this method to work without sleeping
+    #
+    # This is the standard way of creating an application object. It will
+    # launch the app if it is not already running and then create the
+    # accessibility object.
+    #
+    # However, this method is a HUGE hack in cases where the app is not
+    # already running; I've tried to register for notifications, launch
+    # synchronously, etc., but there is always a problem with accessibility
+    # not being ready. Hopefully this problem will go away on Lion...
+    #
     # @param [String] bundle
+    # @param [Float] timeout how long to wait between polling
     # @return [AX::Application]
     def self.application_with_bundle_identifier bundle, sleep_time = 2
       while (apps = NSRunningApplication.runningApplicationsWithBundleIdentifier bundle).empty?
@@ -19,8 +31,10 @@ class Application < AX::Element
       application_for_pid apps.first.processIdentifier
     end
 
+    ##
     # You can call this method to create the application object if the app is
     # already running; otherwise the object creation will fail.
+    #
     # @param [Fixnum] pid The process identifier for the application you want
     # @return [AX::Application]
     def self.application_for_pid pid
@@ -30,7 +44,9 @@ class Application < AX::Element
 
     private
 
+    ##
     # This method uses asynchronous method calls to launch applications.
+    #
     # @param [String] bundle the bundle identifier for the app
     # @return [Boolean]
     def self.launch_application bundle
@@ -43,14 +59,23 @@ class Application < AX::Element
   end
 
 
-  # The inherited {Element#get_focus} will not work for applications.
-  # @param [String] title the title of the application in the dock
-  # @return [Boolean] true if successful, otherwise false
+  ##
+  # @todo This method needs a fall back procedure if the app does not
+  #       have a dock icon
+  #
+  # The inherited {Element#get_focus} will not work for applications,
+  # so we will just get focus by "clicking" the dock icon for the app.
+  #
+  # @return [Boolean] true if successful, otherwise unpredictable
   def get_focus
     AX::DOCK.list.application_dock_item(title: title).press
   end
 
+  ##
   # Create and return a notification observer for the object's application.
+  # This method is almost never directly called, it is instead called by
+  # {Traits::Notifications#wait_for_notification}.
+  #
   # @param [Proc] callback
   # @return [AXObserverRef]
   def observer callback
@@ -59,6 +84,7 @@ class Application < AX::Element
     observer[0]
   end
 
+  ##
   # Override the base class to make sure the pid is included
   def inspect
     (super).sub />$/, "@pid=#{self.pid}>"
