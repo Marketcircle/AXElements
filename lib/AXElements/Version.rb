817f78fece7afab76fb2e52d4b08c95dcbc80447
Mark Rada <mrada@marketcircle.com>
2011-03-16 20:06:01 -0400

    Update load path adjusting to only add vendor/
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- lib/AXElements/Version.rb
+++ lib/AXElements/Version.rb
@@ -5,16 +5,11 @@ module AX
 
   # A list of dependencies
   RUNTIME_DEPENDENCIES = {
-    i18n:          '0.5.0',
-    activesupport: '3.0.5'
+    'i18n'          => '0.5.0',
+    'activesupport' => '3.0.5'
   }
-
-  # Point of entry for any runtime dependencies
-  GEM_PATH = File.absolute_path "#{File.dirname __FILE__}/../../gems"
 
   # Specify load paths to get around needing rubygems
-  RUNTIME_DEPENDENCIES.each_pair { |dep, version|
-    $LOAD_PATH.unshift "#{GEM_PATH}/#{dep}-#{version}/lib"
-  }
+  $LOAD_PATH.unshift File.absolute_path("#{File.dirname __FILE__}/../../vendor")
 
 end
