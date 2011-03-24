cf1723563b884cfb947a475db85b6959b227b430
Mark Rada <mrada@marketcircle.com>
2011-03-24 16:05:42 -0400

    Comment out skipped test
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- test/test_application.rb
+++ test/test_application.rb
@@ -8,18 +8,18 @@ class TestAXApplication < MiniTest::Unit::TestCase
     assert AX::DOCK.respond_to?(:post_kb_event)
   end
 
-  def test_application_with_bundle_identifier_should_launch_app_if_not_running
-    skip 'This test is too invasive, need to find another way or add a test option'
-    # while true
-    #   mails = NSRunningApplication.runningApplicationsWithBundleIdentifier 'com.apple.mail'
-    #   break if mails.empty?
-    #   mails.first.terminate
-    # end
+  # def test_application_with_bundle_identifier_should_launch_app_if_not_running
+  #   # skip 'This test is too invasive, need to find another way or add a test option'
+  #   # while true
+  #   #   mails = NSRunningApplication.runningApplicationsWithBundleIdentifier 'com.apple.mail'
+  #   #   break if mails.empty?
+  #   #   mails.first.terminate
+  #   # end
 
-    # AX::Application.application_with_bundle_identifier 'com.apple.mail'
-    # mails = NSRunningApplication.runningApplicationsWithBundleIdentifier 'com.apple.mail'
-    # mails.should_not be_empty
-  end
+  #   # AX::Application.application_with_bundle_identifier 'com.apple.mail'
+  #   # mails = NSRunningApplication.runningApplicationsWithBundleIdentifier 'com.apple.mail'
+  #   # mails.should_not be_empty
+  # end
 
   def test_inspect_includes_pid
     assert_match /\s@pid=/, AX::DOCK.inspect
