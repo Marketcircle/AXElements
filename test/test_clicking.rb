ff209fc410c0bab2d69674d5f91f30fef6e6a522
Mark Rada <mrada@marketcircle.com>
2011-04-08 17:43:02 -0400

    Get rid of (mostly) empty test files
    The couple of tests being thrown out will need to be rewritten anyways.
    
    
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- test/test_clicking.rb
+++
@@ -1,8 +0,0 @@
-class TestAXElement < MiniTest::Unit::TestCase
-  def test_can_click
-    assert AX::Element.instance_methods.include?(:left_click)
-  end
-end
-
-class TestAXElementClicking < MiniTest::Unit::TestCase
-end
--- test/test_notifications.rb
+++
@@ -1,8 +0,0 @@
-class TestAXElement < MiniTest::Unit::TestCase
-  def test_can_wait_for_notifications
-    assert AX::Element.instance_methods.include?(:wait_for_notification)
-  end
-end
-
-class TestAXElementNotifications < MiniTest::Unit::TestCase
-end
