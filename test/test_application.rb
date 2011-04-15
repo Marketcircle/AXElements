cdcdd3f5f18a8cd33840a5b1186ef4422cb99458
Mark Rada <mrada@marketcircle.com>
2011-04-15 12:56:18 -0400

    Update tests for AX::Application
    Still some things that need to be implemented.
    
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- test/test_application.rb
+++ test/test_application.rb
@@ -1,28 +1,34 @@
 class TestAXApplication < MiniTest::Unit::TestCase
 
+
   def test_is_subclass_of_element
     assert AX::Application.ancestors.include?(AX::Element)
   end
 
-  def test_can_post_keyboard_events
-    assert AX::DOCK.respond_to?(:post_kb_event)
-  end
+  # # @todo mocking is almost not worth it
+  # def test_application_with_bundle_identifier
+  #   def dup_class.AX
 
-  # def test_application_with_bundle_identifier_should_launch_app_if_not_running
-  #   # skip 'This test is too invasive, need to find another way or add a test option'
-  #   # while true
-  #   #   mails = NSRunningApplication.runningApplicationsWithBundleIdentifier 'com.apple.mail'
-  #   #   break if mails.empty?
-  #   #   mails.first.terminate
-  #   # end
-
-  #   # AX::Application.application_with_bundle_identifier 'com.apple.mail'
-  #   # mails = NSRunningApplication.runningApplicationsWithBundleIdentifier 'com.apple.mail'
-  #   # mails.should_not be_empty
+  #   end
+  #   dup_class.application_with_bundle_identifier 'com.apple.dock'
+  #   # assert mock
   # end
+
+  def test_set_attribute_has_special_case_for_focus
+    dup_class  = AX::Application.dup
+    was_called = false
+    dup_class.send :define_method, :set_focus do was_called = true end
+    AX::DOCK.set_attribute :focused, true
+    assert was_called
+  end
 
   def test_inspect_includes_pid
     assert_match /\s@pid=/, AX::DOCK.inspect
+  end
+
+  # @todo make this test stronger
+  def test_can_post_keyboard_events
+    assert AX::Application.instance_methods.include?(:post_kb_string)
   end
 
 end
