76d08ad09df4d6371238d832326e6f5ddb950bf6
Mark Rada <mrada@marketcircle.com>
2011-04-15 16:47:08 -0400

    Raise an error instead of exiting when AXElements cannot load
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- lib/AXElements.rb
+++ lib/AXElements.rb
@@ -5,13 +5,14 @@ framework 'Cocoa'
 # check that the Accessibility APIs are enabled
 begin
   unless AXAPIEnabled()
-    NSLog('The Accessibility APIs are disabled on this machine.')
-    NSLog('Please enable the Accessibility APIs from the System Preferences')
-    exit 3
+    raise RuntimeError, <<-EOS
+Universal Access is disabled on this machine. Please enable it in the System Preferences.
+    EOS
   end
 rescue NoMethodError
-  NSLog('You need to install the latest BridgeSupport preview for CoreFoundation access')
-  exit 4
+  raise NotImplementedError, <<-EOS
+You need to install the latest BridgeSupport preview for AXElements to work.
+  EOS
 end
 
 require 'AXElements/Version'
