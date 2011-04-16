d7b74a468865b6d7e6da937dce4295d6065503e0
Mark Rada <mrada@marketcircle.com>
2011-04-16 18:47:34 -0400

    Move some tests around
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- test/test_ax_constants.rb
+++ test/test_ax_constants.rb
@@ -1,14 +1,3 @@
-class TestAXPrefix < MiniTest::Unit::TestCase
-  def test_removes_ax_prefix; prefix_test 'AXButton', 'Button'; end
-  def test_removes_combination_prefixes; prefix_test 'MCAXButton', 'Button'; end
-  def test_works_with_all_caps; prefix_test 'AXURL', 'URL'; end
-  def test_works_with_long_name; prefix_test 'AXIsApplicationRunning', 'IsApplicationRunning'; end
-  def test_is_not_greedy; prefix_test 'AXMAX', 'MAX'; end
-  def prefix_test before, after
-    assert_equal after, AX.strip_prefix(before)
-  end
-end
-
 class TestAXConstants < MiniTest::Unit::TestCase
   def test_system_is_the_system_wide_object
     assert_instance_of AX::SystemWide, AX::SYSTEM
--- test/test_ax_singletons.rb
+++ test/test_ax_singletons.rb
@@ -278,3 +278,14 @@ class TestAXLogAXCall < TestAX
     assert_match /Not Implemented/, @log_output.string
   end
 end
+
+class TestAXPrefix < MiniTest::Unit::TestCase
+  def test_removes_ax_prefix; prefix_test 'AXButton', 'Button'; end
+  def test_removes_combination_prefixes; prefix_test 'MCAXButton', 'Button'; end
+  def test_works_with_all_caps; prefix_test 'AXURL', 'URL'; end
+  def test_works_with_long_name; prefix_test 'AXIsApplicationRunning', 'IsApplicationRunning'; end
+  def test_is_not_greedy; prefix_test 'AXMAX', 'MAX'; end
+  def prefix_test before, after
+    assert_equal after, AX.strip_prefix(before)
+  end
+end
