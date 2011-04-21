73df8cc96b9ae4b66cb7cf11eab4bf94937501b6
Mark Rada <mrada@marketcircle.com>
2011-04-21 16:26:36 -0400

    Begin splitting test suite
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

---
+++ test/elements/test_search.rb
@@ -0,0 +1,25 @@
+class TestAXElement < MiniTest::Unit::TestCase
+  def test_can_search
+    assert AX::Element.instance_methods.include?(:search)
+  end
+end
+
+class TestAXElementSearch < MiniTest::Unit::TestCase
+  def test_search_one_level_deep
+    assert_equal 'AX::List', AX::DOCK.search(:list).class.to_s
+  end
+  def test_search_multiple_levels_deep
+    assert_equal 'AX::ApplicationDockItem', AX::DOCK.search(:application_dock_item).class.to_s
+  end
+  def test_search_works_with_plural
+    ret = AX::DOCK.search(:lists)
+    assert_instance_of Array, ret
+    assert_instance_of AX::List, ret.first
+  end
+end
+
+# @todo test #search
+# is breadth first
+# plural works
+# singular works
+# assert performance
