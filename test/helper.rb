2f369bc38a49d03580a2f76228a8d7cec7a0d004
Mark Rada <mrada@marketcircle.com>
2011-04-21 15:54:23 -0400

    Use the minitest/pride that ships with minitest-macruby-pride
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- test/helper.rb
+++ test/helper.rb
@@ -1,9 +1,9 @@
 require 'AXElements'
-
 require 'StringIO'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'vendor_test'))
 require 'minitest/autorun'
+require 'minitest/pride'
 
 $init_output = StringIO.new
 AX.log = Logger.new $init_output
@@ -14,35 +14,3 @@ class MiniTest::Unit::TestCase
     AX.log = Logger.new @log_output
   end
 end
-
-
-##
-# Import of minitest/pride from minitest-macruby-pride
-class PrideIO
-  attr_reader :io
-  COLORS = (31..36).to_a
-  COLORS_SIZE = COLORS.size
-
-  def initialize io
-    @io = io
-    @index = 0
-  end
-
-  def print o
-    case o
-    when "." then
-      @index += 1
-      io.print "\e[#{COLORS[@index % COLORS_SIZE]}m*\e[0m"
-    when "E", "F" then
-      io.print "\e[41m\e[37m#{o}\e[0m"
-    else
-      io.print o
-    end
-  end
-
-  def method_missing msg, *args
-    io.send(msg, *args)
-  end
-end
-
-MiniTest::Unit.output = PrideIO.new(MiniTest::Unit.output)
