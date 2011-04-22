35cb9e191ef59434e947f693be0ce1c4d6f4c95b
Mark Rada <mrada@marketcircle.com>
2011-04-21 20:42:14 -0400

    Rewrite tests for attribute related stuff in the core
    Mostly composed of old tests, but relying on lower level code instead of code from
    a higher level of abstraction.
    
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

---
+++ test/core/helper.rb
@@ -0,0 +1,80 @@
+##
+# In order to test the core, I have had to independently recreate some
+# of the functionality in the core so that I can have the proper
+# arguments to use with methods calls.
+class TestAX < MiniTest::Unit::TestCase
+
+  def self.pid_for_app name
+    APPS.find { |app| app.localizedName == name }.processIdentifier
+  end
+
+  # returns raw attribute
+  def self.attribute_for element, attr
+    ptr = Pointer.new(:id)
+    AXUIElementCopyAttributeValue( element, attr, ptr )
+    ptr[0]
+  end
+
+  def self.action_for element, action
+    AXUIElementPerformAction( element, action )
+  end
+
+  def self.element_at_pos point
+    ptr     = Pointer.new( '^{__AXUIElement}' )
+    system  = AXUIElementCreateSystemWide()
+    AXUIElementCopyElementAtPosition( system, point.x, point.y, ptr )
+    ptr[0]
+  end
+
+  APPS       = NSWorkspace.sharedWorkspace.runningApplications
+  DOCK_PID   = pid_for_app 'Dock'
+  FINDER_PID = pid_for_app 'Finder'
+  SYSTEM     = AXUIElementCreateSystemWide()
+  DOCK       = AXUIElementCreateApplication(DOCK_PID)
+  FINDER     = AXUIElementCreateApplication(FINDER_PID)
+  LIST       = attribute_for( DOCK, KAXChildrenAttribute ).first
+
+  def attribute_for element, attr
+    self.class.attribute_for element, attr
+  end
+
+  def children_for element
+    attribute_for element, KAXChildrenAttribute
+  end
+
+  def action_for element, action
+    self.class.action_for element, action
+  end
+
+  def element_at_pos point
+    self.class.element_at_pos point
+  end
+
+  def finder_dock_item
+    children_for( LIST ).find do |item|
+      attribute_for( item, KAXTitleAttribute ) == 'Finder'
+    end
+  end
+
+  def spotlight_text_field
+    screen    = NSScreen.mainScreen.frame
+    spotlight = element_at_pos CGPoint.new(screen.size.width - 10, 5)
+    skip 'Could not find spotlight in the menu bar' unless spotlight
+    action_for spotlight, KAXPressAction
+    group = children_for( children_for( children_for( spotlight ).first ).first )
+    text_field = group.find do |element|
+      attribute_for(element, KAXRoleAttribute) == KAXTextFieldRole
+    end
+    yield text_field
+    action_for spotlight, KAXPressAction
+  end
+
+  # turn logging on bet
+  def with_logging level = Logger::DEBUG
+    original_level = AX.log.level
+    AX.log.level   = level
+    yield
+    AX.log.level   = original_level
+  end
+
+end
---
+++ test/core/test_attributes.rb
@@ -0,0 +1,156 @@
+require 'core/helper'
+
+##
+# AFAICT every accessibility object **MUST** have attributes, so
+# there are no tests to check what happens when they do not exist.
+class TestAXAttrsOfElement < TestAX
+
+  def setup
+    @attrs = AX.attrs_of_element(DOCK)
+  end
+
+  def test_returns_array_of_strings
+    assert_instance_of String, @attrs.first
+  end
+
+  def test_make_sure_certain_attributes_are_accessible
+    assert @attrs.include?(KAXRoleAttribute)
+    assert @attrs.include?(KAXRoleDescriptionAttribute)
+  end
+
+  def test_other_attributes_that_the_dock_should_have
+    assert @attrs.include?(KAXChildrenAttribute)
+    assert @attrs.include?(KAXTitleAttribute)
+  end
+
+end
+
+
+class TestAXAttrOfElement < TestAX
+
+  def test_does_not_return_raw_values
+    assert_kind_of AX::Element, AX.attr_of_element(FINDER, KAXMenuBarAttribute)
+  end
+
+  def test_does_not_return_raw_values_in_array
+    ret = AX.attr_of_element(DOCK, KAXChildrenAttribute).first
+    assert_kind_of AX::Element, ret
+  end
+
+  def test_returns_nil_for_non_existant_attributes
+    assert_nil AX.attr_of_element(DOCK, 'MADEUPATTRIBUTE')
+  end
+
+  def test_logs_message_for_non_existant_attributes
+    with_logging do AX.attr_of_element(DOCK, 'MADEUPATTRIBUTE') end
+    assert_match /#{KAXErrorAttributeUnsupported}/, @log_output.string
+  end
+
+  def test_returns_nil_for_nil_attributes
+    assert_nil AX.attr_of_element(DOCK, KAXFocusedUIElementAttribute)
+  end
+
+  def test_returns_boolean_false_for_false_attributes
+    assert_equal false, AX.attr_of_element(DOCK, 'AXEnhancedUserInterface')
+  end
+
+  def test_returns_boolean_true_for_true_attributes
+    ret = AX.attr_of_element(finder_dock_item, KAXIsApplicationRunningAttribute)
+    assert_equal true, ret
+  end
+
+  def test_wraps_axuielementref_objects
+    ret = AX.attr_of_element(FINDER, KAXMenuBarAttribute)
+    assert_instance_of AX::MenuBar, ret
+  end
+
+  def test_returns_array_for_array_attributes
+    ret = AX.attr_of_element(DOCK, KAXChildrenAttribute)
+    assert_kind_of Array, ret
+  end
+
+  def test_returned_arrays_are_not_empty_when_they_should_have_stuff
+    refute_empty AX.attr_of_element(DOCK, KAXChildrenAttribute)
+  end
+
+  def test_returns_number_for_number_attribute
+    separator = children_for( LIST ).find { |item|
+      attribute_for( item, KAXDescriptionAttribute ) == 'separator'
+    }
+    skip 'You need to have a dock separator for this test' unless separator
+    assert_kind_of NSNumber, AX.attr_of_element(separator, KAXValueAttribute)
+  end
+
+  def test_returns_array_of_numbers_when_attribute_has_an_array_of_numbers
+    skip 'I have no idea where to find an attribute that has this type'
+  end
+
+  def test_returns_a_cgsize_for_size_attributes
+    mb = AX.attr_of_element(FINDER, KAXMenuBarAttribute)
+    assert_instance_of CGSize, mb.get_attribute(:size)
+  end
+
+  def test_returns_a_cgpoint_for_point_attributes
+    mb = AX.attr_of_element(FINDER, KAXMenuBarAttribute)
+    assert_instance_of CGPoint, mb.get_attribute(:position)
+  end
+
+  # @todo have to go deep to find a reliable source of ranges
+  def test_returns_a_cfrange_for_range_attributes
+    spotlight_text_field do |field|
+      range = AX.attr_of_element(field, KAXSelectedTextRangeAttribute)
+      assert_instance_of CFRange, range
+    end
+  end
+
+  # @todo AXAPI supports this, but I cannont find what type of element
+  #       has an attribute stored as a CGRect in the documentation or
+  #       in my experience
+  # def test_returns_a_cgrect_for_rect_attributes
+  # end
+
+  def test_works_with_strings
+    assert_kind_of NSString, AX.attr_of_element(DOCK, KAXTitleAttribute)
+  end
+
+end
+
+class TestAXElementAttributeWritable < TestAX
+
+  def test_true_for_writable_attribute
+    assert AX.attr_of_element_writable?(finder_dock_item, KAXSelectedAttribute)
+  end
+
+  def test_false_for_non_writable_attribute
+    refute AX.attr_of_element_writable?(DOCK, KAXTitleAttribute)
+  end
+
+  def test_false_for_non_existante_attribute
+    refute AX.attr_of_element_writable?(DOCK, 'FAKE')
+  end
+
+  # # @todo this test fails because I am not getting the expected result code
+  # def test_logs_errors
+  #   with_logging do AX.attr_of_element_writable?(DOCK, 'OMG') end
+  #   assert_match /#{KAXErrorAttributeUnsupported}/, @log_output.string
+  # end
+
+end
+
+class TestAXSetAttrOfElement < TestAX
+
+  # @todo these tests require me to go deep into a UI
+
+  def test_set_a_text_fields_value
+    spotlight_text_field do |field|
+      new_value = "#{Time.now}"
+      AX.set_attr_of_element( field, KAXValueAttribute, new_value )
+      assert_equal new_value, attribute_for( field, KAXValueAttribute )
+    end
+  end
+
+  # @todo not sure how to do this without doing a lot of work
+  # def test_set_a_radio_button
+  # end
+
+end
