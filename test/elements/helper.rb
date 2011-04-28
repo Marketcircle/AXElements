f09ab724de3950654009e6ea4158aeab7e8a132a
Mark Rada <mrada@marketcircle.com>
2011-04-27 22:43:39 -0400

    Expose random application dock item for generic testing
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- test/elements/helper.rb
+++ test/elements/helper.rb
@@ -1,5 +1,9 @@
 class TestElements < TestAX
 
   EL_DOCK = AX.application_for_pid(DOCK_PID)
+  EL_DOCK_APP =
+    AX.attr_of_element(LIST, KAXChildrenAttribute).find { |x|
+    x.class == AX::ApplicationDockItem
+  }
 
 end
