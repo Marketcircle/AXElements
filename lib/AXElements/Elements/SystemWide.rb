2b3f6c647e370e4f45f6ebdeec728703ac266104
Mark Rada <mrada@marketcircle.com>
2011-04-29 20:03:49 -0400

    Change SystemWide to inherit from Element
    It only needs the #type_string method which has already been refactored into the AX
    singleton methods.
    
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- lib/AXElements/Elements/SystemWide.rb
+++ lib/AXElements/Elements/SystemWide.rb
@@ -1,5 +1,20 @@
 module AX
-class SystemWide < AX::Application
+
+##
+# @todo Make this class `include Singleton`?
+# @todo Remove methods we know this class cannot use (e.g. param_attributes)
+#
+# Represents the special SystemWide accessibility object.
+class SystemWide < AX::Element
+
+  ##
+  # @note With the SystemWide class, using {#type_string} will send the
+  #       events to which ever app has focus.
+  #
+  # Generate keyboard events by simulating keyboard input.
+  def type_string string
+    AX.keyboard_action( @ref, string )
+  end
 
 end
 end
