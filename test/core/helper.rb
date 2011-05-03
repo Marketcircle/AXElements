1e47a3a66da3f2ab34d889b05b1bb79870f2560b
Mark Rada <mrada@marketcircle.com>
2011-05-03 18:46:03 -0400

    The main test helper needs to know about the spotlight stuff
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- test/core/helper.rb
+++ test/core/helper.rb
@@ -1,18 +1,5 @@
 class TestCore < TestAX
 
-  def spotlight_text_field
-    screen    = NSScreen.mainScreen.frame
-    spotlight = element_at_pos CGPoint.new(screen.size.width - 10, 5)
-    skip 'Could not find spotlight in the menu bar' unless spotlight
-    action_for spotlight, KAXPressAction
-    group = children_for( children_for( children_for( spotlight ).first ).first )
-    text_field = group.find do |element|
-      attribute_for(element, KAXRoleAttribute) == KAXTextFieldRole
-    end
-    yield text_field
-    action_for spotlight, KAXPressAction
-  end
-
   def set_attribute_for element, attribute, value
     AXUIElementSetAttributeValue( element, attribute, value )
   end
--- test/helper.rb
+++ test/helper.rb
@@ -70,6 +70,19 @@ class TestAX < MiniTest::Unit::TestCase
     end
   end
 
+  def spotlight_text_field
+    screen    = NSScreen.mainScreen.frame
+    spotlight = element_at_pos CGPoint.new(screen.size.width - 10, 5)
+    skip 'Could not find spotlight in the menu bar' unless spotlight
+    action_for spotlight, KAXPressAction
+    group = children_for( children_for( children_for( spotlight ).first ).first )
+    text_field = group.find do |element|
+      attribute_for(element, KAXRoleAttribute) == KAXTextFieldRole
+    end
+    yield text_field
+    action_for spotlight, KAXPressAction
+  end
+
   # turn on full logging, yield, set log level back to previous
   def with_logging level = Logger::DEBUG
     original_level = AX.log.level
