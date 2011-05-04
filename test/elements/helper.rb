aa4a4752bfb9a2bd463ca7d6ee5184a6a36a07ef
Mark Rada <mrada@marketcircle.com>
2011-05-04 15:52:15 -0400

    Need a system object for testing
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- test/elements/helper.rb
+++ test/elements/helper.rb
@@ -1,9 +1,12 @@
 class TestElements < TestAX
 
   EL_DOCK = AX.application_for_pid(DOCK_PID)
+
   EL_DOCK_APP =
     AX.attr_of_element(LIST, KAXChildrenAttribute).find { |x|
     x.class == AX::ApplicationDockItem
   }
+
+  EL_SYSTEM = AX::SystemWide.new SYSTEM
 
 end
