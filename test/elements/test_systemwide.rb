5cfef707f0718c1d4017eed019090846cf12380e
Mark Rada <mrada@marketcircle.com>
2011-05-08 03:02:38 -0400

    Test the SystemWide class as it stands
    Signed-off-by: Mark Rada <mrada@marketcircle.com>

--- test/elements/test_systemwide.rb
+++ test/elements/test_systemwide.rb
@@ -1,7 +1,17 @@
 class TestAXSystemWide < MiniTest::Unit::TestCase
 
-  def test_typed_things_go_where_the_focus_is
-    skip 'Pending.'
+  def test_makes_appropriate_callback
+    class << AX
+      alias_method :old_keyboard_action, :keyboard_action
+      def keyboard_action element, string
+        true if string == 'test'
+      end
+    end
+    assert AX::SYSTEM.type_string('test')
+  ensure
+    class << AX
+      alias_method :keyboard_action, :old_keyboard_action
+    end
   end
 
 end
