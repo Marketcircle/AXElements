fad919a83d4b9987f76ed09ee063a641b75d12d4
Mark Rada <mrada@marketcircle.com>
2011-06-23 13:29:49 -0400

    Remove duplication Application.application_with_bundle_identifier

--- lib/AXElements/Elements/Application.rb
+++ lib/AXElements/Elements/Application.rb
@@ -3,23 +3,6 @@
 class AX::Application < AX::Element
 
   ##
-  # This is the standard way of creating an application object. It will
-  # launch the app if it is not already running and then create the
-  # accessibility object.
-  #
-  # However, this method is a HUGE hack in cases where the app is not
-  # already running; I've tried to register for notifications, launch
-  # synchronously, etc., but there is always a problem with accessibility
-  # not being ready. Hopefully this problem will go away on Lion...
-  #
-  # @param [String] bundle
-  # @param [Float] sleep_time how long to wait between polling
-  # @return [AX::Application]
-  def self.application_with_bundle_identifier bundle, sleep_time = 2
-    Accessibility.application_with_bundle_identifier(bundle, sleep_time)
-  end
-
-  ##
   # Overriden to handle the {Kernel#set_focus} case.
   def set_attribute attr, value
     return set_focus if attr == :focused
