bd87a81b284f8ad36c8b04706bf5b856b91e5e99
Mark Rada <mrada@marketcircle.com>
2011-06-29 20:32:45 -0400

    Template for testing Tree

---
+++ test/test_accessibility_tree.rb
@@ -0,0 +1,12 @@
+# This will not be fun...
+
+# first element is root? no, it is child
+
+# iterating through each should go through the full tree, shallow
+# iterating through each should go through the full tree, thin
+# iterating through each should go through the full tree, wide
+# iterating through each should go through the full tree, deep
+
+# find should break when item is found, but how do we test this?
+
+# iterator should know what level we are at (for visual printing)
