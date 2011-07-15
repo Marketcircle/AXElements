878ba1da6d6c2ce2a847c05799e1420a22b1efb1
Mark Rada <mrada@marketcircle.com>
2011-07-15 14:55:14 -0400

    Outline tests for search

---
+++ test/accessibility/test_search.rb
@@ -0,0 +1,45 @@
+class TestSearch < TestAX
+
+  def test_search_one_level_deep
+  end
+
+  def test_search_two_levels_deep
+  end
+
+  def test_search_with_no_filters
+  end
+
+  def test_search_with_one_filter
+  end
+
+  def test_search_with_two_filters
+  end
+
+  def test_find_returns_element_when_found
+  end
+
+  def test_find_returns_nil_when_nothing_found
+  end
+
+  def test_find_all_returns_array_with_elements_with_found
+  end
+
+  def test_find_all_returns_empty_array_when_nothing_found
+  end
+
+  def test_filter_is_attribute_not_available_on_all_instances
+    # might have to add an attribute to a button or something
+  end
+
+  def test_searching_for_superclass_will_also_find_subclasses_as_well
+  end
+
+  def test_searching_for_subclasses_will_not_find_superclasses
+  end
+
+  def test_title_ui_element_filtering
+    # case 1: classes match
+    # case 2: classes do not match
+  end
+
+end
