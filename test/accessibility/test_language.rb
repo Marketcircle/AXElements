a73bf4baea60025b5cca47618138606f679780e9
Mark Rada <mrada@marketcircle.com>
2011-07-15 15:08:26 -0400

    Outline tests for Language

---
+++ test/accessibility/test_language.rb
@@ -0,0 +1,79 @@
+class TestAccessibilityLanguage < TestAX
+
+  def test_set_focus
+  end
+
+  def test_set_arbitrary_attribute
+  end
+
+  def test_set_value
+  end
+
+  def test_raise
+  end
+
+  def test_arbitrary_action
+  end
+
+  def test_method_missing_still_forwards
+    # technically we are using a workaround right now
+  end
+
+  def test_type_sends_to_system_by_default
+  end
+
+  def test_type_can_send_to_arbitrary_applications
+  end
+
+  def test_register_for_notification_forwards_to_element
+  end
+
+  def test_wait_for_notification_forwards
+  end
+
+  def test_wait_for_notification_has_default
+  end
+
+  def test_move_mouse_to_accepts_many_inputs_but_forwards_cgpoint
+  end
+
+  def test_drag_mouse_accepts_many_inputs_but_forwards_cgpoint
+  end
+
+  def test_scroll_moves_mouse_to_object_first_if_given
+  end
+
+  def test_scroll_forwards_properly
+  end
+
+  def test_click_moves_mouse_to_object_first
+  end
+
+  def test_click_forwards
+  end
+
+  def test_right_click_moves_mouse_to_object_first
+  end
+
+  def test_right_click_forwards
+  end
+
+  def test_right_click_alias
+  end
+
+  def test_double_click_moves_mouse_to_object_first
+  end
+
+  def test_double_click_forwards
+  end
+
+  def test_show_about_window_for
+  end
+
+  def test_preferences_window_for
+  end
+
+  def test_is_mixed_into_toplevel
+  end
+
+end
