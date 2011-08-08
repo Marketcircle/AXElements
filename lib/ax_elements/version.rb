efd08c9d535d5173cbc64e6151c72e06ca174088
Mark Rada <mrada@marketcircle.com>
2011-08-08 14:37:01 -0400

    Add type data for VERSION constant and friends
    Boost the documentation percentage...

--- lib/ax_elements/version.rb
+++ lib/ax_elements/version.rb
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 module Accessibility
+  # @return [String]
   VERSION   = '0.5.0'
+
+  # @return [String]
   CODE_NAME = 'PokéMaster'
+
+  # @return [String]
   RELEASE   = 'Luxray'
 end
