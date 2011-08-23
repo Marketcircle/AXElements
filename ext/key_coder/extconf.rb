519495ac6c680432d20e35f0f860dc653d5c4439
Mark Rada <mrada@marketcircle.com>
2011-08-22 22:56:58 -0400

    Whitespace

--- ext/key_coder/extconf.rb
+++ ext/key_coder/extconf.rb
@@ -1,6 +1,6 @@
 require 'mkmf'
 
 $CFLAGS << ' -std=c99 -fobjc-gc -Wall -Werror'
-$LIBS << ' -framework Cocoa -framework CoreServices -framework Carbon'
+$LIBS   << ' -framework Cocoa -framework CoreServices -framework Carbon'
 
 create_makefile('key_coder')
