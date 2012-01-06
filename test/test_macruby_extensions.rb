810431bb7f7a735b6f1d85c41e9359311d98d415
Mark Rada <mrada@marketcircle.com>
2012-01-06 17:26:42 -0500

    Remove NSString#map

--- test/test_macruby_extensions.rb
+++ test/test_macruby_extensions.rb
@@ -75,14 +75,6 @@ class NSStringExtensions < MiniTest::Unit::TestCase
     assert_equal 'Name',        'Name'.camelize
   end
 
-  def test_map
-    ret = NSString.stringWithString('hello').map { |_| true }
-    assert_equal [true, true, true, true, true], ret
-
-    ret = NSString.stringWithString('hello').map { |_| [true] }
-    assert_equal [true, true, true, true, true], ret
-  end
-
 end
 
 
