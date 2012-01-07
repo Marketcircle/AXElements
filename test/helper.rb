a29617e097c0354c15c7484629ea8f3ea5f46ac8
Mark Rada <mrada@marketcircle.com>
2012-01-07 00:23:39 -0500

    Adjust test/helper.rb according to changes to Accessibility::Core

--- test/helper.rb
+++ test/helper.rb
@@ -8,6 +8,7 @@ require 'stringio'
 # We want to launch the test app and make sure it responds to
 # accessibility queries, but that is difficult, so we just sleep
 APP_BUNDLE_URL = NSURL.fileURLWithPath File.expand_path './test/fixture/Release/AXElementsTester.app'
+APP_BUNDLE_IDENTIFIER = 'com.marketcircle.AXElementsTester'
 
 error = Pointer.new :id
 TEST_APP = NSWorkspace.sharedWorkspace.launchApplicationAtURL APP_BUNDLE_URL,
@@ -36,62 +37,41 @@ else
   require'minitest/pride'
 end
 
-
 class MiniTest::Unit::TestCase
-  # You may need this to help track down an issue if a test is crashing MacRuby
-  # def self.test_order
-  #   :alpha
-  # end
-
   def assert_instance_of_boolean value
     message = "Expected #{value.inspect} to be a boolean"
     assert value.is_a?(TrueClass) || value.is_a?(FalseClass), message
   end
 
-  def self.bench_range
-    bench_exp 10, 10_000
-  end
-end
-
-module AXHelpers
-  def pid_for name
+  def pid_from name # sneaky naming
     NSWorkspace.sharedWorkspace.runningApplications.find do |app|
       app.bundleIdentifier == name
     end.processIdentifier
   end
 
-  # returns raw attribute
-  def attribute_for element, attr
-    ptr = Pointer.new :id
-    AXUIElementCopyAttributeValue(element, attr, ptr)
-    ptr[0]
+  # You may need this to help track down an issue if a test is crashing MacRuby
+  # def self.test_order
+  #   :alpha
+  # end
+
+  def self.bench_range
+    bench_exp 10, 10_000
   end
 
+  PID = pid_from APP_BUNDLE_IDENTIFIER
+  REF = AXUIElementCreateApplication(PID)
+end
+
+##
+# Conveniences for Accessibility::Core
+module CoreHelpers
   def children_for element
-    attribute_for element, KAXChildrenAttribute
+    attr KAXChildrenAttribute, for: element
   end
 
   def value_for element
-    attribute_for element, KAXValueAttribute
+    attr KAXValueAttribute, for: element
   end
-
-  def action_for element, action
-    AXUIElementPerformAction(element, action)
-  end
-
-  # remember to wrap structs in an AXValueRef
-  def set_attribute_for element, attr, value
-    AXUIElementSetAttributeValue(element, attr, value)
-  end
-end
-
-class TestAX < MiniTest::Unit::TestCase
-  include AXHelpers
-  extend AXHelpers
-
-  APP_BUNDLE_IDENTIFIER = 'com.marketcircle.AXElementsTester'
-  PID = pid_for APP_BUNDLE_IDENTIFIER
-  REF = AXUIElementCreateApplication(PID)
 end
 
 ##
