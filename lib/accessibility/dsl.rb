77022553f6965c9f5c5f499f7206c9b0d950feea
Mark Rada <mrada@marketcircle.com>
2012-01-11 17:00:04 -0500

    Hide some of the backtrace when errors come from DSL#method_missing

--- lib/accessibility/dsl.rb
+++ lib/accessibility/dsl.rb
@@ -29,7 +29,7 @@ module Accessibility::DSL
       # should be able to just call super, but there is a bug in MacRuby (#1320)
       # so we just recreate what should be happening
       message = "undefined method `#{method}' for #{self}:#{self.class}"
-      raise NoMethodError, message
+      raise NoMethodError, message, caller(1)
     end
     arg.perform method
   end
