1211c3a0f3f0c8f44be995d48ffb7af5ab34d62b
Mark Rada <mrada@marketcircle.com>
2012-01-12 12:40:28 -0500

    Leave the error class overrides turned on

--- lib/ax_elements/macruby_extensions.rb
+++ lib/ax_elements/macruby_extensions.rb
@@ -200,41 +200,41 @@ class NilClass
 end
 
 
-# ##
-# # Correct a problem with ArgumentError not providing a proper backtrace.
-# class ArgumentError
-#   alias_method :original_message, :message
-#   def message
-#     "#{original_message}\n\t#{backtrace.join("\n\t")}"
-#   end
-# end
+##
+# Correct a problem with ArgumentError not providing a proper backtrace.
+class ArgumentError
+  alias_method :original_message, :message
+  def message
+    "#{original_message}\n\t#{backtrace.join("\n\t")}"
+  end
+end
 
 
-# ##
-# # Correct a problem with NameError not providing a proper backtrace.
-# class NameError
-#   alias_method :original_message, :message
-#   def message
-#     "#{original_message}\n\t#{backtrace.join("\n\t")}"
-#   end
-# end
+##
+# Correct a problem with NameError not providing a proper backtrace.
+class NameError
+  alias_method :original_message, :message
+  def message
+    "#{original_message}\n\t#{backtrace.join("\n\t")}"
+  end
+end
 
 
-# ##
-# # Correct a problem with NameError not providing a proper backtrace.
-# class RuntimeError
-#   alias_method :original_message, :message
-#   def message
-#     "#{original_message}\n\t#{backtrace.join("\n\t")}"
-#   end
-# end
+##
+# Correct a problem with NameError not providing a proper backtrace.
+class RuntimeError
+  alias_method :original_message, :message
+  def message
+    "#{original_message}\n\t#{backtrace.join("\n\t")}"
+  end
+end
 
 
-# ##
-# # Correct a problem with NoMethodError not providing a proper backtrace.
-# class NoMethodError
-#   alias_method :original_message, :message
-#   def message
-#     "#{original_message}\n\t#{backtrace.join("\n\t")}"
-#   end
-# end
+##
+# Correct a problem with NoMethodError not providing a proper backtrace.
+class NoMethodError
+  alias_method :original_message, :message
+  def message
+    "#{original_message}\n\t#{backtrace.join("\n\t")}"
+  end
+end
