3cd8eef3cfb536de2fd2f1429c7928d9d4edcce9
Mark Rada <mrada@marketcircle.com>
2012-01-23 14:46:14 -0500

    Force filters to be an empty hash if they are not a kind of hash
    To avoid errors in cases where a search had no filters or any other silly error case.

--- lib/ax/element/errors.rb
+++ lib/ax/element/errors.rb
@@ -16,7 +16,7 @@ module Accessibility
     include Accessibility::Debug
 
     def initialize searcher, searchee, filters
-      raise TypeError, 'filters must be a hash' unless filters.kind_of? Hash
+      filters = {} unless filters.kind_of? Hash
       msg  = "Could not find `#{searchee}#{pp_filters(filters)}` "
       msg << "as a child of #{searcher.class}\n"
       msg << "Element Path:\n\t" << path(searcher)
