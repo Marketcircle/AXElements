701b4b256cbd4d2449756499a7270fab16bc9127
Mark Rada <mrada@marketcircle.com>
2012-01-31 16:53:04 -0500

    Refactor some error handling in error_handler.rb

--- lib/accessibility/core/error_handler.rb
+++ lib/accessibility/core/error_handler.rb
@@ -5,37 +5,38 @@ module Accessibility::Core
 
   # @param [Number]
   def handle_error code, *args
+    description = args.first.description if args.first
     case code
     when KAXErrorIllegalArgument
       msg = case args.size
             when 1
-              "'#{CFCopyDescription(args.first)}' is not an AXUIElementRef"
+              "'#{description}' is not an AXUIElementRef"
             when 2
-              "Either the element '#{CFCopyDescription(args.first)}' " +
+              "Either the element '#{description}' " +
                 "or the attr/action/callback '#{args.second}' is not a legal argument"
             when 3
               "You can't set '#{args.second}' to '#{CFCopyDescription(args.third)}' " +
-                "for '#{CFCopyDescription(args.first)}'"
+                "for '#{description}'"
             when 4
               "The point [#{args.second}, #{args.third}] is not a valid point, or " +
-                "'#{CFCopyDescription(args.first)}' is not an AXUIElementRef"
+                "'#{description}' is not an AXUIElementRef"
             when 5
               "Either the observer '#{CFCopyDescription(args.third)}', " +
-                "the element '#{CFCopyDescription(args.first)}', or " +
+                "the element '#{description}', or " +
                 "the notification '#{args.second}' is not a legitimate argument"
             end
       raise ArgumentError, msg
     when KAXErrorInvalidUIElement
-      msg = "'#{CFCopyDescription(args.first)}' is no longer a valid token"
+      msg = "'#{description}' is no longer a valid token"
       raise RuntimeError, msg
     when KAXErrorAttributeUnsupported
-      msg = "'#{CFCopyDescription(args.first)}' doesn't have '#{args.second}'"
+      msg = "'#{description}' doesn't have '#{args.second}'"
       raise ArgumentError, msg
     when KAXErrorActionUnsupported
-      msg = "'#{CFCopyDescription(args.first)}' doesn't have '#{args.second}'"
+      msg = "'#{description}' doesn't have '#{args.second}'"
       raise ArgumentError, msg
     when KAXErrorParameterizedAttributeUnsupported then
-      msg = "'#{CFCopyDescription(args.first)}' does not have parameterized attributes"
+      msg = "'#{description}' does not have parameterized attributes"
       raise ArgumentError, msg
     when KAXErrorFailure
       msg = 'Some kind of system failure occurred, stopping to be safe'
@@ -43,7 +44,7 @@ module Accessibility::Core
     when KAXErrorCannotComplete
       handle_cannot_complete args.first
     when KAXErrorNotImplemented
-      msg  = "The program that owns '#{CFCopyDescription(args.first)}' "
+      msg  = "The program that owns '#{description}' "
       msg << 'does not work with AXAPI properly'
       raise NotImplementedError, msg
     when KAXErrorNotEnoughPrecision
@@ -55,17 +56,17 @@ module Accessibility::Core
       msg << 'was never valid'
       raise ArgumentError, msg
     when KAXErrorNotificationUnsupported
-      msg = "'#{CFCopyDescription(args.first)}' doesn't support notifications"
+      msg = "'#{description}' doesn't support notifications"
       raise ArgumentError, msg
     when KAXErrorNotificationAlreadyRegistered
       # @todo Does this really neeed to raise an exception? Seems
       #       like a warning would be sufficient.
       msg  = "You have already registered to hear about '#{args.second}' "
-      msg << "from '#{CFCopyDescription(args.first)}'"
+      msg << "from '#{description}'"
       raise ArgumentError, msg
     when KAXErrorNotificationNotRegistered
-      msg  = "You have not yet registered to hear about '#{notif}' "
-      msg << "from '#{CFCopyDescription(element)}'"
+      msg  = "You have not yet registered to hear about '#{args.second}' "
+      msg << "from '#{description}'"
       raise RuntimeError, msg
     else
       raise "You should never reach this line! [#{code.inspect}]"
