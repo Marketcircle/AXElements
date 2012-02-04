7a38b199e251ee2fd820580ed648ae39df8c39e5
Mark Rada <mrada@marketcircle.com>
2012-02-04 03:57:00 -0500

    Update the tests for Accessibility::Enumerators

---
+++ test/accessibility/test_enumerators.rb
@@ -0,0 +1,118 @@
+class TestAccessibilityEnumeratorsBreadthFirst < MiniTest::Unit::TestCase
+  include Accessibility::Core
+
+  def app
+    @@app ||= AX::Application.new REF, attrs_for(REF)
+  end
+
+  def test_each_iterates_in_correct_order
+    tab_group = app.main_window.children.find { |x| x.class == AX::TabGroup }
+    enum      = Accessibility::Enumerators::BreadthFirst.new(tab_group)
+    actual    = enum.map &:class
+    expected  = [
+                 AX::RadioButton, AX::RadioButton, AX::TabGroup,
+                 AX::RadioButton, AX::RadioButton, AX::TabGroup,
+                 AX::RadioButton, AX::RadioButton, AX::TabGroup,
+                 AX::RadioButton, AX::RadioButton, AX::Group,
+                 AX::TextField,   AX::TextField,
+                 AX::StaticText,  AX::StaticText
+                ]
+    assert_equal expected, actual
+  end
+
+  def test_first_element_is_first_child # not the root
+    enum = Accessibility::Enumerators::BreadthFirst.new app
+    def enum.first
+      each { |x| return x }
+    end
+    assert_instance_of AX::StandardWindow, enum.first
+  end
+
+  # this was a big performance issue, so we should make sure it
+  # doesn't happen again
+  def test_find_returns_immediately_after_finding
+    tree  = Accessibility::Enumerators::BreadthFirst.new app
+    cache = []
+    tree.find do |element|
+      cache << element.class
+      element.class == AX::StandardWindow
+    end
+    assert_equal 1, cache.size
+  end
+
+  def bench_each
+    skip 'TODO'
+  end
+
+  def bench_find
+    skip 'TODO'
+  end
+
+end
+
+
+class TestAccessibilityEnumeratorsDepthFirst < MiniTest::Unit::TestCase
+  include Accessibility::Core
+
+  def app
+    @@app ||= AX::Application.new REF, attrs_for(REF)
+  end
+
+  def test_each_iterates_in_correct_order
+    tab_group = app.main_window.children.find { |x| x.class == AX::TabGroup }
+    enum      = Accessibility::Enumerators::DepthFirst.new tab_group
+    actual    = enum.map &:class
+    expected  = [
+                 AX::RadioButton, AX::RadioButton, AX::TabGroup,
+                 AX::RadioButton, AX::RadioButton, AX::TabGroup,
+                 AX::RadioButton, AX::RadioButton, AX::TabGroup,
+                 AX::RadioButton, AX::RadioButton, AX::Group,
+                 AX::TextField, AX::StaticText,
+                 AX::TextField, AX::StaticText
+                ]
+    assert_equal expected, actual
+  end
+
+  # since we have a different implementation, we should also verify order here...
+  def test_each_with_level_in_correct_order
+    tab_group = app.main_window.children.find { |x| x.class == AX::TabGroup }
+    enum      = Accessibility::Enumerators::DepthFirst.new tab_group
+    actual    = []
+    enum.each_with_level do |x,_| actual << x.class end
+    expected  = [
+                 AX::RadioButton, AX::RadioButton, AX::TabGroup,
+                 AX::RadioButton, AX::RadioButton, AX::TabGroup,
+                 AX::RadioButton, AX::RadioButton, AX::TabGroup,
+                 AX::RadioButton, AX::RadioButton, AX::Group,
+                 AX::TextField, AX::StaticText,
+                 AX::TextField, AX::StaticText
+                ]
+    assert_equal expected, actual
+  end
+
+  def test_each_with_level_has_correct_height
+    enum = Accessibility::Enumerators::DepthFirst.new app.main_window
+    enum.each_with_level do |element, level|
+      # msg = element.inspect
+      case element.class.to_s
+      when 'AX::CloseButton','AX::ZoomButton','AX::Slider','AX::RadioGroup',
+           'AX::ScrollArea','AX::CheckBox','AX::Incrementor','AX::SearchField'
+        assert_equal 1, level
+      when 'AX::WebArea','AX::Table','AX::ScrollBar'
+        assert_equal 2, level
+      when 'AX::SortButton'
+        assert_equal 4, level
+      end
+    end
+  end
+
+  def bench_each
+    skip 'TODO'
+  end
+
+  def bench_each_with_level
+    skip 'TODO'
+  end
+
+end
+
