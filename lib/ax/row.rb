cc552937a7e09a57b3a9f888456872e2cfc8f39d
Mark Rada <mrada@marketcircle.com>
2012-02-06 01:13:12 -0500

    Optimize Row#child_in_column

--- lib/ax/row.rb
+++ lib/ax/row.rb
@@ -1,31 +1,36 @@
 require 'ax/element'
+require 'accessibility/qualifier'
 
 ##
 # UI Element for the row in a table, outline, etc.
 class AX::Row < AX::Element
 
   ##
-  # @todo Can this be more efficient? It wraps the columns twice,
-  #       which is expensive if there are many columns. Needs to be
-  #       done in such that we preserve search semantics for the
-  #       filters.
-  #
   # Retrieve the child in a row that corresponds to a specific column.
   # You must pass filters here in the same way that you would for a
   # search.
   #
+  # This is useful for tables where it is difficult to identify which
+  # row item is the one you want based on the row items themselves.
+  # Often times the columns in the table will have identifying attributes,
+  # such as a header, and so you can use this method to figure out what
+  # column your row item is in and then the method will return the row
+  # item you wanted.
+  #
   # @example
   #
-  #   table.row[5].child_in_column(header: 'Price')
+  #   rows  = table.rows
+  #   total = rows.inject(0) { |sum, row|
+  #     sum += row.child_in_column(header: 'Price').value.to_i
+  #   }
+  #   puts "The average price is $ #{total / rows.size}"
   #
   # @param [Hash]
   # @return [AX::Element]
   def child_in_column filters
-    table   = self.parent
-    column  = table.column(filters)
-    columns = table.columns
-    index   = columns.index { |x| x == column }
-    return self.children.at(index) if index
+    qualifier = Accessibility::Qualifier.new(:Column, filters)
+    column    = self.parent.columns.index { |x| qualifier.qualifies? x }
+    return self.children.at(column) if column
     raise Accessibility::SearchFailure.new(self.parent, 'column', filters)
   end
 
