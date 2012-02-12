814a254796c52260ec23f6a6ddfebc90e7030203
Mark Rada <mrada@marketcircle.com>
2012-02-11 19:20:17 -0500

    Move error_handler.rb code into core.rb

--- lib/accessibility/core.rb
+++ lib/accessibility/core.rb
@@ -1,4 +1,4 @@
-require 'accessibility/core/error_handler'
+# -*- coding: utf-8 -*-
 
 ##
 # @todo I feel a bit weird having to instantiate a new pointer every
@@ -538,6 +538,120 @@ module Accessibility::Core
     handle_error code, element, seconds
   end
 
+
+  # @group Error Handling
+
+  # @param [Number]
+  def handle_error code, *args
+    args[0]              = args[0].description if args[0]
+    klass, handler, argc = AXERROR[code] || [RuntimeError]
+    msg                  = if handler
+                             self.send handler, *args[argc]
+                           else
+                             "You should never reach this line [#{code}]"
+                           end
+    raise klass, msg, caller(1)
+  end
+
+  # @private
+  def handle_failure ref
+    "A system failure occurred with #{ref}, stopping to be safe"
+  end
+
+  # @private
+  def handle_illegal_argument *args
+    case args.size
+    when 1
+      "#{args.first} is not an AXUIElementRef"
+    when 2
+      "Either the element #{args.first} " +
+        "or the attr/action/callback #{args.second.inspect} " +
+        'is not a legal argument'
+    when 3
+      "You can't set #{args.second.inspect} to " +
+        "#{args.third.description.inspect} for #{args.first}"
+    when 4
+      "The point [#{args.second}, #{args.third}] is not a valid point, " +
+        "or #{args.first} is not an AXUIElementRef"
+    when 5
+      "Either the observer #{args.third.description}, " +
+        "the element #{args.first}, or " +
+        "the notification #{args.second.inspect} " +
+        "is not a legitimate argument"
+    end
+  end
+
+  # @private
+  def handle_invalid_element ref
+    "#{ref} is no longer a valid reference"
+  end
+
+  # @private
+  def handle_invalid_observer ref, lol, observer
+    "#{observer.description} is no longer a valid observer for #{ref}, or was never valid"
+  end
+
+  # @private
+  # @param [AXUIElementRef]
+  def handle_cannot_complete ref
+    spin_run_loop
+    pid = pid_for ref
+    app = NSRunningApplication.runningApplicationWithProcessIdentifier pid
+    if app
+      "Some unspecified error occurred using #{ref} with AXAPI, possibly a timeout. :("
+    else
+      "Application for pid=#{pid} is no longer running. Maybe it crashed?"
+    end
+  end
+
+  # @private
+  def handle_attr_unsupported ref, attr
+    "#{ref} does not have a #{attr.inspect} attribute"
+  end
+
+  # @private
+  def handle_action_unsupported ref, action
+    "#{ref} does not have a #{action.inspect} action"
+  end
+
+  # @private
+  def handle_notif_unsupported ref, notif
+    "#{ref} does not support the #{notif.inspect} notification"
+  end
+
+  # @private
+  def handle_not_implemented ref
+    "The program that owns #{ref} does not work with AXAPI properly"
+  end
+
+  ##
+  # @private
+  # @todo Does this really neeed to raise an exception? Seems
+  #       like a warning would be sufficient.
+  def handle_notif_registered ref, notif
+    "You have already registered to hear about #{notif.inspect} from #{ref}"
+  end
+
+  # @private
+  def handle_notif_not_registered ref, notif
+    "You have not registered to hear about #{notif.inspect} from #{ref}"
+  end
+
+  # @private
+  def handle_api_disabled
+    'AXAPI has been disabled'
+  end
+
+  # @private
+  def handle_param_attr_unsupported ref, attr
+    "#{ref} does not have a #{attr.inspect} parameterized attribute"
+  end
+
+  # @private
+  def handle_not_enough_precision
+    'AXAPI said there was not enough precision ¯\(°_o)/¯'
+  end
+
   # @endgroup
 
 
@@ -603,9 +717,39 @@ module Accessibility::Core
   ##
   # @private
   #
-  # Perf hack.
+  # Mapping of `AXError` values to static information on how to handle
+  # the error. Used by {handle_error}.
   #
-  # @return [Array(String,String)]
-  ROLES = [KAXSubroleAttribute, KAXRoleAttribute]
+  # @return [Hash{Number=>Array(Symbol,Range)}]
+  AXERROR = {
+    KAXErrorFailure                           =>
+      [RuntimeError,        :handle_failure,                0...1],
+    KAXErrorIllegalArgument                   =>
+      [ArgumentError,       :handle_illegal_argument,       0..-1],
+    KAXErrorInvalidUIElement                  =>
+      [ArgumentError,       :handle_invalid_element,        0...1],
+    KAXErrorInvalidUIElementObserver          =>
+      [ArgumentError,       :handle_invalid_observer,       0...3],
+    KAXErrorCannotComplete                    =>
+      [RuntimeError,        :handle_cannot_complete,        0...1],
+    KAXErrorAttributeUnsupported              =>
+      [ArgumentError,       :handle_attr_unsupported,       0...2],
+    KAXErrorActionUnsupported                 =>
+      [ArgumentError,       :handle_action_unsupported,     0...2],
+    KAXErrorNotificationUnsupported           =>
+      [ArgumentError,       :handle_notif_unsupported,      0...2],
+    KAXErrorNotImplemented                    =>
+      [NotImplementedError, :handle_not_implemented,        0...1],
+    KAXErrorNotificationAlreadyRegistered     =>
+      [ArgumentError,       :handle_notif_registered,       0...2],
+    KAXErrorNotificationNotRegistered         =>
+      [RuntimeError,        :handle_notif_not_registered,   0...2],
+    KAXErrorAPIDisabled                       =>
+      [RuntimeError,        :handle_api_disabled,           0...0],
+    KAXErrorParameterizedAttributeUnsupported =>
+      [ArgumentError,       :handle_param_attr_unsupported, 0...2],
+    KAXErrorNotEnoughPrecision                =>
+      [RuntimeError,        :handle_not_enough_precision,   0...0]
+  }
 
 end
--- test/accessibility/test_core.rb
+++ test/accessibility/test_core.rb
@@ -1,5 +1,10 @@
+# -*- coding: utf-8 -*-
 class TestAccessibilityCore < MiniTest::Unit::TestCase
   include Accessibility::Core
+
+  def ref
+    @@ref ||= Regexp.new(Regexp.escape(REF.description))
+  end
 
   def window
     @@window ||= attr KAXMainWindowAttribute, for: REF
@@ -437,6 +442,117 @@ class TestAccessibilityCore < MiniTest::Unit::TestCase
     assert_raises ArgumentError do
       set_timeout_to 10, for: nil
     end
+  end
+
+
+
+  def error_handler_test args, should_raise: klass, with_fragments: msgs
+    @@meth ||= Regexp.new "`#{__method__}'$"
+    handle_error *args
+  rescue Exception => e
+    assert_instance_of klass, e, e.inspect
+    assert_match @@meth, e.backtrace.first, e.backtrace
+    msgs.each do |msg|
+      assert_match msg, e.message
+    end
+  end
+
+  def test_has_failsafe_exception
+    error_handler_test [99],
+         should_raise: RuntimeError,
+       with_fragments: [/never reach this line/, /99/]
+  end
+
+  def test_failure
+    error_handler_test [KAXErrorFailure, REF],
+         should_raise: RuntimeError,
+       with_fragments: [/system failure/, ref]
+  end
+
+  def test_illegal_argument
+    skip 'OMG, PLEASE NO'
+  end
+
+  def test_invalid_element
+    error_handler_test [KAXErrorInvalidUIElement, REF],
+         should_raise: ArgumentError,
+       with_fragments: [/no longer a valid reference/, ref]
+  end
+
+  def test_invalid_observer
+    error_handler_test [KAXErrorInvalidUIElementObserver, REF, :pie, :cake],
+         should_raise: ArgumentError,
+       with_fragments: [/no longer a valid observer/, /or was never valid/, ref, /cake/]
+  end
+
+  def test_cannot_complete
+    def pid_for lol
+      NSRunningApplication
+        .runningApplicationsWithBundleIdentifier('com.apple.finder')
+        .first.processIdentifier
+    end
+    error_handler_test [KAXErrorCannotComplete, REF],
+         should_raise: RuntimeError,
+       with_fragments: [/Some unspecified error/, ref, /:\(/]
+
+    def pid_for lol; false; end
+    error_handler_test [KAXErrorCannotComplete, nil],
+         should_raise: RuntimeError,
+       with_fragments: [/Application for pid/, /Maybe it crashed\?/]
+  end
+
+  def test_attr_unsupported
+    error_handler_test [KAXErrorAttributeUnsupported, REF, :cake],
+         should_raise: ArgumentError,
+       with_fragments: [/does not have/, /:cake attribute/, ref]
+  end
+
+  def test_action_unsupported
+    error_handler_test [KAXErrorActionUnsupported, REF, :pie],
+         should_raise: ArgumentError,
+       with_fragments: [/does not have/, /:pie action/, ref]
+  end
+
+  def test_notif_unsupported
+    error_handler_test [KAXErrorNotificationUnsupported, REF, :cheese],
+         should_raise: ArgumentError,
+       with_fragments: [/does not support/, /:cheese notification/, ref]
+  end
+
+  def test_not_implemented
+    error_handler_test [KAXErrorNotImplemented, REF],
+         should_raise: NotImplementedError,
+       with_fragments: [/does not work with AXAPI/, ref]
+  end
+
+  def test_notif_registered
+    error_handler_test [KAXErrorNotificationAlreadyRegistered, REF, :lamp],
+         should_raise: ArgumentError,
+       with_fragments: [/already registered/, /:lamp/, ref]
+  end
+
+  def test_notif_not_registered
+    error_handler_test [KAXErrorNotificationNotRegistered, REF, :peas],
+         should_raise: RuntimeError,
+       with_fragments: [/not registered/, /:peas/, ref]
+  end
+
+  def test_api_disabled
+    error_handler_test [KAXErrorAPIDisabled],
+         should_raise: RuntimeError,
+       with_fragments: [/AXAPI has been disabled/]
+  end
+
+  def test_param_attr_unsupported
+    error_handler_test [KAXErrorParameterizedAttributeUnsupported, REF, :oscar],
+         should_raise: ArgumentError,
+       with_fragments: [/does not have/, /:oscar parameterized attribute/, ref]
+  end
+
+  def test_not_enough_precision
+    error_handler_test [KAXErrorNotEnoughPrecision],
+         should_raise: RuntimeError,
+       with_fragments: [/not enough precision/, "¯\(°_o)/¯"]
   end
 
 end
