3448a533c6f73b1429a9fd9c064854ce4d513ebe
Mark Rada <mrada@marketcircle.com>
2012-02-28 14:42:59 -0500

    Implement GH-24

--- test/unit/ax/test_element.rb
+++ test/unit/ax/test_element.rb
@@ -7,6 +7,15 @@ class TestAXElement < MiniTest::Unit::TestCase
   def test_methods_is_flat
     assert_equal element.methods, element.methods.flatten
   end
+
+  def test_setting_through_method_missing
+    got_callback = false
+    element.define_singleton_method :'set:to:' do |attr, value|
+      if attr == 'my_little_pony' && value == :firefly
+        got_callback = true
+      end
+    end
+    assert element.my_little_pony = :firefly
   end
 
 end
