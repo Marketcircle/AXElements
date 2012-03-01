a76ee651bed65f33b724643ffc471e749a8a1b64
Mark Rada <mrada@marketcircle.com>
2012-03-01 00:33:37 -0500

    Test that Mouse movement goes where it should

---
+++ test/unit/test_mouse.rb
@@ -0,0 +1,19 @@
+class TestMouseModule < MiniTest::Unit::TestCase
+
+  def distance point1, point2
+    x = point1.x - point2.x
+    y = point1.y - point2.y
+    Math.sqrt((x**2) + (y**2))
+  end
+
+  def test_move_to
+    point = CGPointMake(100, 100)
+    Mouse.move_to point
+    assert_in_delta 0, distance(point,Mouse.current_position), 1.0
+
+    point = CGPointMake(rand(700)+150, rand(500)+100)
+    Mouse.move_to point
+    assert_in_delta 0, distance(point,Mouse.current_position), 1.0
+  end
+
+end
