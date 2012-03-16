1a47e274dc2383f32bbdc1c5e74a830133e74d61
Mark Rada <mrada@marketcircle.com>
2012-03-16 01:48:40 -0400

    Remove duplicated NSDictionary monkey patches

--- test/unit/test_macruby_extensions.rb
+++ test/unit/test_macruby_extensions.rb
@@ -1,15 +1,3 @@
-class TestNSDictionaryExtensions < MiniTest::Unit::TestCase
-
-  def test_ax_pp
-    assert_equal ::EMPTY_STRING,            {}.ax_pp
-    assert_equal '(title: "Hey, listen!")', {title: 'Hey, listen!'}.ax_pp
-    assert_equal '(a: 42, b: [3.14])',      {a: 42, b: [3.14]}.ax_pp
-    assert_equal '(c(d: ":("))',            {c: {d:':('} }.ax_pp
-  end
-
-end
-
-
 class TestNSArrayExtensions < MiniTest::Unit::TestCase
 
   def test_second_returns_second
