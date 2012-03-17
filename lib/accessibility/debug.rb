0f6127ab2ca6148d49c4d62d6c48eeb31fca1cff
Mark Rada <mrada@marketcircle.com>
2012-03-17 01:24:23 -0400

    Move CGRect#flip! into debug.rb since only debug.rb uses it

--- lib/accessibility/debug.rb
+++ lib/accessibility/debug.rb
@@ -145,3 +145,24 @@ module Accessibility::Debug
 
   end
 end
+
+
+##
+# AXElements extensions to `CGRect`.
+class CGRect
+  ##
+  # Treats the rect as belonging to one co-ordinate system and then
+  # converts it to the other system.
+  #
+  # This is useful because accessibility API's expect to work with
+  # the flipped co-ordinate system (origin in top left), but AppKit
+  # prefers to use the cartesian co-ordinate system (origin in bottom
+  # left).
+  #
+  # @return [CGRect]
+  def flip!
+    screen_height = NSMaxY(NSScreen.mainScreen.frame)
+    origin.y      = screen_height - NSMaxY(self)
+    self
+  end
+end
--- test/unit/accessibility/test_debug.rb
+++ test/unit/accessibility/test_debug.rb
@@ -44,3 +44,17 @@ class TestAccessibilityDebug < MiniTest::Unit::TestCase
   end
 
 end
+
+class TestCGRectExtensions < MiniTest::Unit::TestCase
+
+  def test_flipping
+    size = NSScreen.mainScreen.frame.size
+    assert_equal CGRectMake(  0,     size.height,  0,  0), CGRectZero.dup.flip!
+    assert_equal CGRectMake(100, size.height-200,100,100), CGRectMake(100,100,100,100).flip!
+  end
+
+  def test_flipping_twice_returns_to_original
+    assert_equal CGRectZero.dup, CGRectZero.dup.flip!.flip!
+  end
+
+end
