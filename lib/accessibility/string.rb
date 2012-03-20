48b1fc658f0a352d7037e9a74a23008ea5748848
Mark Rada <mrada@marketcircle.com>
2012-03-20 10:21:36 -0400

    Move some String constants in EventGenerator
    So they are localized to where they are used.

--- lib/accessibility/string.rb
+++ lib/accessibility/string.rb
@@ -9,228 +9,9 @@ require 'accessibility/key_code_generator'
 # international layouts as well.
 module Accessibility::String
 
-  ##
-  # Regenerate the portion of the key mapping that is set dynamically based
-  # on keyboard layout (e.g. US, Dvorak, etc.).
-  #
-  # This method should be called whenever the keyboard layout changes. If a run
-  # loop is spinning then it will automatically be called.
-  def self.regenerate_dynamic_mapping
-    # KeyCodeGenerator is declared in the Objective-C extension
-    MAPPING.merge! KeyCodeGenerator.dynamic_mapping
-    MAPPING["\n"] = MAPPING["\r"]
-  end
-
 
   private
 
-  ##
-  # @private
-  #
-  # Dynamic mapping of characters to keycodes. The map is generated at
-  # startup time in order to support multiple keyboard layouts.
-  #
-  # @return [Hash{String=>Fixnum}]
-  MAPPING = {}
-
-  ##
-  # @note Static values come from `/System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/Headers/Events.h`
-  #
-  # Map of custom escape sequences to their hardcoded keycode value.
-  #
-  # @return [Hash{String=>Fixnum}]
-  CUSTOM = {
-    'ESCAPE'          => 0x35,
-    'ESC'             => 0x35,
-    'COMMAND'         => 0x37,
-    'CMD'             => 0x37,
-    'SHIFT'           => 0x38,
-    'LSHIFT'          => 0x38,
-    'CAPS'            => 0x39,
-    'CAPSLOCK'        => 0x39,
-    'OPTION'          => 0x3A,
-    'OPT'             => 0x3A,
-    'ALT'             => 0x3A,
-    'CONTROL'         => 0x3B,
-    'CTRL'            => 0x3B,
-    'RSHIFT'          => 0x3C,
-    'ROPTION'         => 0x3D,
-    'ROPT'            => 0x3D,
-    'RALT'            => 0x3D,
-    'RCONTROL'        => 0x3E,
-    'RCTRL'           => 0x3E,
-    'FUNCTION'        => 0x3F,
-    'FN'              => 0x3F,
-    'VOLUMEUP'        => 0x48,
-    'VOLUP'           => 0x48,
-    'VOLUMEDOWN'      => 0x49,
-    'VOLDOWN'         => 0x49,
-    'MUTE'            => 0x4A,
-    'F1'              => 0x7A,
-    'F2'              => 0x78,
-    'F3'              => 0x63,
-    'F4'              => 0x76,
-    'F5'              => 0x60,
-    'F6'              => 0x61,
-    'F7'              => 0x62,
-    'F8'              => 0x64,
-    'F9'              => 0x65,
-    'F10'             => 0x6D,
-    'F11'             => 0x67,
-    'F12'             => 0x6F,
-    'F13'             => 0x69,
-    'F14'             => 0x6B,
-    'F15'             => 0x71,
-    'F16'             => 0x6A,
-    'F17'             => 0x40,
-    'F18'             => 0x4F,
-    'F19'             => 0x50,
-    'F20'             => 0x5A,
-    'HELP'            => 0x72,
-    'HOME'            => 0x73,
-    'END'             => 0x77,
-    'PAGEUP'          => 0x74,
-    'PAGEDOWN'        => 0x79,
-    'DELETE'          => 0x75,
-    'LEFT'            => 0x7B,
-    '<-'              => 0x7B,
-    'RIGHT'           => 0x7C,
-    '->'              => 0x7C,
-    'DOWN'            => 0x7D,
-    'UP'              => 0x7E,
-    '0'               => 0x52,
-    '1'               => 0x53,
-    '2'               => 0x54,
-    '3'               => 0x55,
-    '4'               => 0x56,
-    '5'               => 0x57,
-    '6'               => 0x58,
-    '7'               => 0x59,
-    '8'               => 0x5B,
-    '9'               => 0x5C,
-    'Decimal'         => 0x41,
-    '.'               => 0x41,
-    'Plus'            => 0x45,
-    '+'               => 0x45,
-    'Multiply'        => 0x43,
-    '*'               => 0x43,
-    'Minus'           => 0x4E,
-    '-'               => 0x4E,
-    'Divide'          => 0x4B,
-    '/'               => 0x4B,
-    'Equals'          => 0x51,
-    '='               => 0x51,
-    'Enter'           => 0x4C,
-    'Clear'           => 0x47,
-  }
-
-  ##
-  # Mapping of shifted (characters written when holding shift) characters
-  # to keycodes.
-  #
-  # @return [Hash{String=>Fixnum}]
-  SHIFTED = {
-    '~'               => '`',
-    '!'               => '1',
-    '@'               => '2',
-    '#'               => '3',
-    '$'               => '4',
-    '%'               => '5',
-    '^'               => '6',
-    '&'               => '7',
-    '*'               => '8',
-    '('               => '9',
-    ')'               => '0',
-    '{'               => '[',
-    '}'               => ']',
-    '?'               => '/',
-    '+'               => '=',
-    '|'               => "\\",
-    ':'               => ';',
-    '_'               => '-',
-    '"'               => "'",
-    '<'               => ',',
-    '>'               => '.',
-    'A'               => 'a',
-    'B'               => 'b',
-    'C'               => 'c',
-    'D'               => 'd',
-    'E'               => 'e',
-    'F'               => 'f',
-    'G'               => 'g',
-    'H'               => 'h',
-    'I'               => 'i',
-    'J'               => 'j',
-    'K'               => 'k',
-    'L'               => 'l',
-    'M'               => 'm',
-    'N'               => 'n',
-    'O'               => 'o',
-    'P'               => 'p',
-    'Q'               => 'q',
-    'R'               => 'r',
-    'S'               => 's',
-    'T'               => 't',
-    'U'               => 'u',
-    'V'               => 'v',
-    'W'               => 'w',
-    'X'               => 'x',
-    'Y'               => 'y',
-    'Z'               => 'z',
-  }
-
-  ##
-  # Mapping of optioned (characters written when holding option/alt)
-  # characters to keycodes.
-  #
-  # @return [Hash{String=>Fixnum}]
-  OPTIONED = {
-    '¡'               => '1',
-    '™'               => '2',
-    '£'               => '3',
-    '¢'               => '4',
-    '∞'               => '5',
-    '§'               => '6',
-    '¶'               => '7',
-    '•'               => '8',
-    'ª'               => '9',
-    'º'               => '0',
-    '“'               => '[',
-    '‘'               => ']',
-    'æ'               => "'",
-    '≤'               => ',',
-    '≥'               => '.',
-    'π'               => 'p',
-    '¥'               => 'y',
-    'ƒ'               => 'f',
-    '©'               => 'g',
-    '®'               => 'r',
-    '¬'               => 'l',
-    '÷'               => '/',
-    '≠'               => '=',
-    '«'               => "\\",
-    'å'               => 'a',
-    'ø'               => 'o',
-    '´'               => 'e',
-    '¨'               => 'u',
-    'ˆ'               => 'i',
-    '∂'               => 'd',
-    '˙'               => 'h',
-    '†'               => 't',
-    '˜'               => 'n',
-    'ß'               => 's',
-    '–'               => '-',
-    '…'               => ';',
-    'œ'               => 'q',
-    '∆'               => 'j',
-    '˚'               => 'k',
-    '≈'               => 'x',
-    '∫'               => 'b',
-    'µ'               => 'm',
-    '∑'               => 'w',
-    '√'               => 'v',
-    'Ω'               => 'z',
-  }
 
   ##
   # Tokenizer for strings. This class will take a string and break
@@ -292,9 +73,233 @@ module Accessibility::String
     CUSTOM_ESCAPE = "\\"
   end
 
+
   ##
-  # Generate a sequence of keyboard events given tokens.
+  # Generate a sequence of keyboard events given a sequence of tokens.
   class EventGenerator
+
+    ##
+    # Regenerate the portion of the key mapping that is set dynamically based
+    # on keyboard layout (e.g. US, Dvorak, etc.).
+    #
+    # This method should be called whenever the keyboard layout changes. This
+    # can be called automatically by registering for a notification in a run
+    # looped environment.
+    def self.regenerate_dynamic_mapping
+      # KeyCodeGenerator is declared in the Objective-C extension
+      MAPPING.merge! KeyCodeGenerator.dynamic_mapping
+      MAPPING["\n"] = MAPPING["\r"]
+    end
+
+    ##
+    # @private
+    #
+    # Dynamic mapping of characters to keycodes. The map is generated at
+    # startup time in order to support multiple keyboard layouts.
+    #
+    # @return [Hash{String=>Fixnum}]
+    MAPPING = {}
+
+    ##
+    # @note These mappings are all static and come from `/System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/Headers/Events.h`
+    #
+    # Map of custom escape sequences to their hardcoded keycode value.
+    #
+    # @return [Hash{String=>Fixnum}]
+    CUSTOM = {
+      'ESCAPE'          => 0x35,
+      'ESC'             => 0x35,
+      'COMMAND'         => 0x37,
+      'CMD'             => 0x37,
+      'SHIFT'           => 0x38,
+      'LSHIFT'          => 0x38,
+      'CAPS'            => 0x39,
+      'CAPSLOCK'        => 0x39,
+      'OPTION'          => 0x3A,
+      'OPT'             => 0x3A,
+      'ALT'             => 0x3A,
+      'CONTROL'         => 0x3B,
+      'CTRL'            => 0x3B,
+      'RSHIFT'          => 0x3C,
+      'ROPTION'         => 0x3D,
+      'ROPT'            => 0x3D,
+      'RALT'            => 0x3D,
+      'RCONTROL'        => 0x3E,
+      'RCTRL'           => 0x3E,
+      'FUNCTION'        => 0x3F,
+      'FN'              => 0x3F,
+      'VOLUMEUP'        => 0x48,
+      'VOLUP'           => 0x48,
+      'VOLUMEDOWN'      => 0x49,
+      'VOLDOWN'         => 0x49,
+      'MUTE'            => 0x4A,
+      'F1'              => 0x7A,
+      'F2'              => 0x78,
+      'F3'              => 0x63,
+      'F4'              => 0x76,
+      'F5'              => 0x60,
+      'F6'              => 0x61,
+      'F7'              => 0x62,
+      'F8'              => 0x64,
+      'F9'              => 0x65,
+      'F10'             => 0x6D,
+      'F11'             => 0x67,
+      'F12'             => 0x6F,
+      'F13'             => 0x69,
+      'F14'             => 0x6B,
+      'F15'             => 0x71,
+      'F16'             => 0x6A,
+      'F17'             => 0x40,
+      'F18'             => 0x4F,
+      'F19'             => 0x50,
+      'F20'             => 0x5A,
+      'HELP'            => 0x72,
+      'HOME'            => 0x73,
+      'END'             => 0x77,
+      'PAGEUP'          => 0x74,
+      'PAGEDOWN'        => 0x79,
+      'DELETE'          => 0x75,
+      'LEFT'            => 0x7B,
+      '<-'              => 0x7B,
+      'RIGHT'           => 0x7C,
+      '->'              => 0x7C,
+      'DOWN'            => 0x7D,
+      'UP'              => 0x7E,
+      '0'               => 0x52,
+      '1'               => 0x53,
+      '2'               => 0x54,
+      '3'               => 0x55,
+      '4'               => 0x56,
+      '5'               => 0x57,
+      '6'               => 0x58,
+      '7'               => 0x59,
+      '8'               => 0x5B,
+      '9'               => 0x5C,
+      'Decimal'         => 0x41,
+      '.'               => 0x41,
+      'Plus'            => 0x45,
+      '+'               => 0x45,
+      'Multiply'        => 0x43,
+      '*'               => 0x43,
+      'Minus'           => 0x4E,
+      '-'               => 0x4E,
+      'Divide'          => 0x4B,
+      '/'               => 0x4B,
+      'Equals'          => 0x51,
+      '='               => 0x51,
+      'Enter'           => 0x4C,
+      'Clear'           => 0x47,
+    }
+
+    ##
+    # Mapping of shifted (characters written when holding shift) characters
+    # to keycodes.
+    #
+    # @return [Hash{String=>Fixnum}]
+    SHIFTED = {
+      '~'               => '`',
+      '!'               => '1',
+      '@'               => '2',
+      '#'               => '3',
+      '$'               => '4',
+      '%'               => '5',
+      '^'               => '6',
+      '&'               => '7',
+      '*'               => '8',
+      '('               => '9',
+      ')'               => '0',
+      '{'               => '[',
+      '}'               => ']',
+      '?'               => '/',
+      '+'               => '=',
+      '|'               => "\\",
+      ':'               => ';',
+      '_'               => '-',
+      '"'               => "'",
+      '<'               => ',',
+      '>'               => '.',
+      'A'               => 'a',
+      'B'               => 'b',
+      'C'               => 'c',
+      'D'               => 'd',
+      'E'               => 'e',
+      'F'               => 'f',
+      'G'               => 'g',
+      'H'               => 'h',
+      'I'               => 'i',
+      'J'               => 'j',
+      'K'               => 'k',
+      'L'               => 'l',
+      'M'               => 'm',
+      'N'               => 'n',
+      'O'               => 'o',
+      'P'               => 'p',
+      'Q'               => 'q',
+      'R'               => 'r',
+      'S'               => 's',
+      'T'               => 't',
+      'U'               => 'u',
+      'V'               => 'v',
+      'W'               => 'w',
+      'X'               => 'x',
+      'Y'               => 'y',
+      'Z'               => 'z',
+    }
+
+    ##
+    # Mapping of optioned (characters written when holding option/alt)
+    # characters to keycodes.
+    #
+    # @return [Hash{String=>Fixnum}]
+    OPTIONED = {
+      '¡'               => '1',
+      '™'               => '2',
+      '£'               => '3',
+      '¢'               => '4',
+      '∞'               => '5',
+      '§'               => '6',
+      '¶'               => '7',
+      '•'               => '8',
+      'ª'               => '9',
+      'º'               => '0',
+      '“'               => '[',
+      '‘'               => ']',
+      'æ'               => "'",
+      '≤'               => ',',
+      '≥'               => '.',
+      'π'               => 'p',
+      '¥'               => 'y',
+      'ƒ'               => 'f',
+      '©'               => 'g',
+      '®'               => 'r',
+      '¬'               => 'l',
+      '÷'               => '/',
+      '≠'               => '=',
+      '«'               => "\\",
+      'å'               => 'a',
+      'ø'               => 'o',
+      '´'               => 'e',
+      '¨'               => 'u',
+      'ˆ'               => 'i',
+      '∂'               => 'd',
+      '˙'               => 'h',
+      '†'               => 't',
+      '˜'               => 'n',
+      'ß'               => 's',
+      '–'               => '-',
+      '…'               => ';',
+      'œ'               => 'q',
+      '∆'               => 'j',
+      '˚'               => 'k',
+      '≈'               => 'x',
+      '∫'               => 'b',
+      'µ'               => 'm',
+      '∑'               => 'w',
+      '√'               => 'v',
+      'Ω'               => 'z',
+    }
+
+
     def initialize tokens
       @tokens = tokens
       # *4 since the output array will be at least *2 the
@@ -390,10 +395,10 @@ end
 #
 # Register to be notified if the keyboard layout changes at runtime
 # framework 'Carbon'
-# NSDistributedNotificationCenter.defaultCenter.addObserver Accessibility::String,
+# NSDistributedNotificationCenter.defaultCenter.addObserver Accessibility::String::EventGenerator,
 #                                                selector: 'regenerate_dynamic_mapping',
 #                                                    name: KTISNotifySelectedKeyboardInputSourceChanged,
 #                                                  object: nil
 
 # Initialize the table
-Accessibility::String.regenerate_dynamic_mapping
+Accessibility::String::EventGenerator.regenerate_dynamic_mapping
