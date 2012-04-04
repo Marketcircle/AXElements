b718af6e2f1b66ef1332af5c8dcbc64f256dc0f5
Mark Rada <mrada@marketcircle.com>
2012-04-04 12:31:19 -0400

    Remove Accessibility::LookupFailure since we do not use it anymore

--- lib/accessibility/errors.rb
+++ lib/accessibility/errors.rb
@@ -1,43 +1,32 @@
 require 'accessibility/debug'
 
-module Accessibility
+##
+# Error raised when an implicit search fails to return a result.
+class Accessibility::SearchFailure < NoMethodError
 
-  ##
-  # Error raised when a dynamic attribute lookup fails.
-  class LookupFailure < ArgumentError
-    def initialize element, name
-      super "#{name.inspect} was not found for #{element.inspect}"
-    end
+  def initialize searcher, searchee, filters
+    filters = {} unless filters.kind_of? Hash
+    msg  = "Could not find `#{pp_searchee searchee, filters}` "
+    msg << "as a child of #{searcher.class}\n"
+    msg << "Element Path:\n\t" << path_to(searcher)
+    # @todo Consider turning this on by default
+    msg << "\nSubtree:\n\t" << debug(searcher) if Accessibility::Debug.on?
+    super msg
   end
 
-  ##
-  # Error raised when an implicit search fails to return a result.
-  class SearchFailure < NoMethodError
 
-    def initialize searcher, searchee, filters
-      filters = {} unless filters.kind_of? Hash
-      msg  = "Could not find `#{pp_searchee searchee, filters}` "
-      msg << "as a child of #{searcher.class}\n"
-      msg << "Element Path:\n\t" << path_to(searcher)
-      # @todo Consider turning this on by default
-      msg << "\nSubtree:\n\t" << debug(searcher) if Accessibility::Debug.on?
-      super msg
-    end
+  private
 
+  def pp_searchee searchee, filters
+    Accessibility::Qualifier.new(searchee, filters).describe
+  end
 
-    private
+  def path_to element
+    Accessibility::Debug.path(element).map! { |x| x.inspect }.join("\n\t")
+  end
 
-    def pp_searchee searchee, filters
-      Accessibility::Qualifier.new(searchee, filters).describe
-    end
-
-    def path_to element
-      Accessibility::Debug.path(element).map! { |x| x.inspect }.join("\n\t")
-    end
-
-    def debug searcher
-      Accessibility::Debug.text_subtree(searcher)
-    end
+  def debug searcher
+    Accessibility::Debug.text_subtree(searcher)
   end
 
 end
--- test/sanity/accessibility/test_errors.rb
+++ test/sanity/accessibility/test_errors.rb
@@ -1,20 +1,7 @@
+require 'test/runner'
+require 'accessibility/errors'
+
 class TestAccessibilityErrors < MiniTest::Unit::TestCase
-
-  def test_lookup_failure_shows_inspect_output_of_arguments
-    e = Accessibility::LookupFailure.new(:element, :name)
-    def e.backtrace; []; end
-    assert_match /:name was not found for :element/, e.message
-
-    o = Object.new
-    def o.inspect; '"I am an object"'; end
-    e = Accessibility::LookupFailure.new(o, [1,2,3])
-    def e.backtrace; []; end
-    assert_match /\[1, 2, 3\] was not found for "I am an object"/, e.message
-  end
-
-  def test_lookup_failue_is_kind_of_arg_error
-    assert_includes Accessibility::LookupFailure.ancestors, ArgumentError
-  end
 
   def test_search_failure_is_kind_of_no_method_error
     assert_includes Accessibility::SearchFailure.ancestors, NoMethodError
