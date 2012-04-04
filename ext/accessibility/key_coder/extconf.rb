6ffc2c4feaa0a9831c7229ac13b13b50fd08026b
Mark Rada <mrada@marketcircle.com>
2012-04-04 11:38:25 -0400

    Be more intelligent about selecting clang as the compiler

--- ext/accessibility/key_coder/extconf.rb
+++ ext/accessibility/key_coder/extconf.rb
@@ -6,9 +6,15 @@ $LIBS   << ' -framework Cocoa -framework Carbon -framework ApplicationServices'
 if RUBY_ENGINE == 'macruby'
   $CFLAGS << ' -fobjc-gc'
 else
+  if clang = `which clang`.chomp
+    unless RbConfig::CONFIG["CC"].match /clang/
+      RbConfig::MAKEFILE_CONFIG["CC"]  = clang
+      RbConfig::MAKEFILE_CONFIG["CXX"] = clang
+    end
+  else
+    $stdout.puts "Clang not installed. Cannot build C extension"
+  end
   $CFLAGS << ' -DNOT_MACRUBY'
-  RbConfig::MAKEFILE_CONFIG["CC"]  = "clang"
-  RbConfig::MAKEFILE_CONFIG["CXX"] = "clang"
 end
 
 create_makefile('accessibility/key_coder')
