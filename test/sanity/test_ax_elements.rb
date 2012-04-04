33b0eeec55a9c5a0602ef3da99ef49c0a5f0012c
Mark Rada <mrada@marketcircle.com>
2012-04-04 14:02:42 -0400

    Make sure we still test that DOCK is defined

---
+++ test/sanity/test_ax_elements.rb
@@ -0,0 +1,8 @@
+class TestDefaults < MiniTest::Unit::TestCase
+
+  def test_dock_constant_is_set
+    assert_instance_of AX::Application, AX::DOCK
+    assert_equal 'Dock', AX::DOCK.title
+  end
+
+end
