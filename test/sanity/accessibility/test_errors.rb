62e723039a83c22269e81c2eadef7827ba637378
Mark Rada <mrada@marketcircle.com>
2012-04-14 13:30:46 -0400

    Make DSL#wait_for raise an exception if a timeout occurs
    This addresses GH-44

--- test/sanity/accessibility/test_errors.rb
+++ test/sanity/accessibility/test_errors.rb
@@ -3,8 +3,9 @@ require 'accessibility/errors'
 
 class TestAccessibilityErrors < MiniTest::Unit::TestCase
 
-  def test_search_failure_is_kind_of_no_method_error
-    assert_includes Accessibility::SearchFailure.ancestors, NoMethodError
+  def test_exception_subclassing
+    assert_equal NoMethodError,                Accessibility::SearchFailure.superclass
+    assert_equal Accessibility::SearchFailure, Accessibility::PollingTimeout.superclass
   end
 
 end
