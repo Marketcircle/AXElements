4e938a3ce000333ecaebb82f10affa65a5771ebc
Mark Rada <mrada@marketcircle.com>
2012-04-19 00:31:15 -0400

    Refactor RSpec matchers and add new polling matchers for GH-49

--- lib/rspec/expectations/ax_elements.rb
+++ lib/rspec/expectations/ax_elements.rb
@@ -1,46 +1,160 @@
+require 'accessibility/dsl'
 require 'accessibility/qualifier'
 require 'ax/element'
 
-##
-# Custom matcher for RSpec to check if an element has the specified
-# child element.
-class Accessibility::HasChildMatcher
+module Accessibility
 
-  # @param [#to_s]
-  # @param [Hash]
-  # @yield
-  def initialize kind, filters, &block
-    @qualifier = Accessibility::Qualifier.new(kind, filters, &block)
+  ##
+  # @abstract
+  #
+  # Base class for RSpec matchers used with AXElements.
+  class AbstractMatcher
+
+    # @return [#to_s]
+    attr_reader :kind
+
+    # @return [Hash{Symbol=>Object}]
+    attr_reader :filters
+
+    # @return [Proc]
+    attr_reader :block
+
+    # @param [#to_s]
+    # @param [Hash]
+    # @yield
+    def initialize kind, filters, &block
+      @kind, @filters, @block = kind, filters, block
+    end
+
+    # @param [AX::Element]
+    def does_not_match? element
+      !matches?(element)
+    end
+
+
+    private
+
+    # @return [Accessibility::Qualifier]
+    def qualifier
+      @qualifier ||= Accessibility::Qualifier.new(kind, filters, &block)
+    end
   end
 
-  # @param [AX::Element]
-  def matches? parent
-    @parent = parent
-    @result = parent.children.find { |x| @qualifier.qualifies? x }
-    !@result.blank?
+  ##
+  # Custom matcher for RSpec to check if an element has the specified
+  # child element.
+  class HasChildMatcher < AbstractMatcher
+    # @param [AX::Element]
+    def matches? parent
+      @parent = parent
+      @result = parent.children.find { |x| qualifier.qualifies? x }
+      !@result.blank?
+    end
+
+    # @return [String]
+    def failure_message_for_should
+      "Expected #@parent to have child #{qualifier.describe}"
+    end
+
+    # @return [String]
+    def failure_message_for_should_not
+      "Expected #@parent to NOT have child #@result"
+    end
+
+    # @return [String]
+    def description
+    "should have a child that matches #{qualifier.describe}"
+    end
   end
 
-  # @return [String]
-  def failure_message_for_should
-    "Expected #@parent to have child #{@qualifier.describe}"
+  ##
+  # Custom matcher for RSpec to check if an element has the specified
+  # descendent element.
+  class HasDescendentMatcher < AbstractMatcher
+    # @param [AX::Element]
+    def matches? ancestor
+      @ancestor = ancestor
+      @result   = ancestor.search(kind, filters, &block)
+      !@result.blank?
+    end
+
+    # @return [String]
+    def failure_message_for_should
+      "Expected #@ancestor to have descendent #{qualifier.describe}"
+    end
+
+    # @return [String]
+    def failure_message_for_should_not
+      "Expected #@ancestor to NOT have descendent #@result"
+    end
+
+    # @return [String]
+    def description
+      "should have a descendent matching #{qualifier.describe}"
+    end
   end
 
-  # @param [AX::Element]
-  def does_not_match? parent
-    !matches?(parent)
+  ##
+  # Custom matcher for RSpec to check if an element has the specified
+  # child element within a grace period. Used for testing things
+  # after an asynchronous action is performed.
+  class HasChildShortlyMatcher < AbstractMatcher
+    include DSL
+
+    # @param [AX::Element]
+    def matches? parent
+      @filters[:parent] = @parent = parent
+      @result = wait_for kind, filters, &block
+      !@result.blank?
+    end
+
+    # @return [String]
+    def failure_message_for_should
+      "Expected #@parent to have child #{qualifier.describe} before a timeout occurred"
+    end
+
+    # @return [String]
+    def failure_message_for_should_not
+      "Expected #@parent to NOT have child #@result before a timeout occurred"
+    end
+
+    # @return [String]
+    def description
+      "should have a child that matches #{qualifier.describe} before a timeout occurs"
+    end
   end
 
-  # @return [String]
-  def failure_message_for_should_not
-    "Expected #@parent to NOT have child #@result"
-  end
+  ##
+  # Custom matcher for RSpec to check if an element has the specified
+  # descendent element within a grace period. Used for testing things
+  # after an asynchronous action is performed.
+  class HasDescendentShortlyMatcher < AbstractMatcher
+    include DSL
 
-  # @return [String]
-  def description
-    "should have a child that matches #{@qualifier.describe}"
-  end
+    # @param [AX::Element]
+    def matches? ancestor
+      @filters[:ancestor] = @ancestor = ancestor
+      @result = wait_for kind, filters, &block
+      !@result.blank?
+    end
 
+    # @return [String]
+    def failure_message_for_should
+      "Expected #@ancestor to have descendent #{qualifier.describe} before a timeout occurred"
+    end
+
+    # @return [String]
+    def failure_message_for_should_not
+      "Expected #@ancestor to NOT have descendent #@result before a timeout occurred"
+    end
+
+    # @return [String]
+    def description
+      "should have a descendent matching #{qualifier.describe} before a timeout occurs"
+    end
+  end
 end
+
 
 ##
 # Assert that the receiving element has the specified child element. You
@@ -61,49 +175,6 @@ def have_child kind, filters = {}, &block
   Accessibility::HasChildMatcher.new kind, filters, &block
 end
 
-
-##
-# Custom matcher for RSpec to check if an element has the specified
-# descendent element.
-class Accessibility::HasDescendentMatcher
-
-  # @param [#to_s]
-  # @param [Hash]
-  # @yield
-  def initialize kind, filters, &block
-    @kind, @filters, @block = kind, filters, block
-    @qualifier = Accessibility::Qualifier.new(@kind, @filters, &@block)
-  end
-
-  # @param [AX::Element]
-  def matches? ancestor
-    @ancestor = ancestor
-    @result   = ancestor.search(@kind, @filters, &@block)
-    !@result.blank?
-  end
-
-  # @return [String]
-  def failure_message_for_should
-    "Expected #@ancestor to have descendent #{@qualifier.describe}"
-  end
-
-  # @param [AX::Element]
-  def does_not_match? ancestor
-    !matches?(ancestor)
-  end
-
-  # @return [String]
-  def failure_message_for_should_not
-    "Expected #@ancestor to NOT have descendent #@result"
-  end
-
-  # @return [String]
-  def description
-    "should have a descendent matching #{@qualifier.describe}"
-  end
-
-end
-
 ##
 # Assert that the given element has the specified descendent. You can
 # pass any parameters you normally would use during a search,
@@ -122,3 +193,42 @@ def have_descendent kind, filters = {}, &block
   Accessibility::HasDescendentMatcher.new kind, filters, &block
 end
 alias :have_descendant :have_descendent
+
+##
+# Assert that the given element has the specified child soon. This
+# method will block until the child is found or a timeout occurs. You
+# can pass any parameters you normally would use during a search,
+# including a block.
+#
+# @example
+#
+#   app.main_window.should shortly_have_child(:row, static_text: { value: 'Cake' })
+#
+#   row.should_not shortly_have_child(:check_box)
+#
+# @param [#to_s]
+# @param [Hash]
+# @yield An optional block to be used as part of the search qualifier
+def shortly_have_child kind, filters = {}, &block
+  Accessibility::HasChildShortlyMatcher.new(kind, filters, &block)
+end
+
+##
+# Assert that the given element has the specified descendent soon. This
+# method will block until the descendent is found or a timeout occurs.
+# You can pass any parameters you normally would use during a search,
+# including a block.
+#
+# @example
+#
+#   app.main_window.should shortly_have_child(:row, static_text: { value: 'Cake' })
+#
+#   row.should_not shortly_have_child(:check_box)
+#
+# @param [#to_s]
+# @param [Hash]
+# @yield An optional block to be used as part of the search qualifier
+def shortly_have_descendent kind, filters = {}, &block
+  Accessibility::HasDescendentShortlyMatcher.new kind, filters, &block
+end
+alias :shortly_have_descendant :shortly_have_descendent
--- test/integration/rspec/expectations/test_ax_elements.rb
+++ test/integration/rspec/expectations/test_ax_elements.rb
@@ -8,51 +8,100 @@ class TestRSpecMatchers < MiniTest::Unit::TestCase
     @@app ||= AX::Application.new PID
   end
 
-  def test_have_child_should_failure_message
-    m = Accessibility::HasChildMatcher.new(:window, {}) {}
-    e = app.main_window.slider
 
-    m.matches? e
-    assert_equal "Expected #{e.inspect} to have child Window[✔]",
-      m.failure_message_for_should
+
+  def test_have_child_should_failure_message
+    e = app.main_window.slider
+    m = have_child(:window)
+
+    refute m.matches?(e)
+    assert_equal "Expected #{e.inspect} to have child Window", m.failure_message_for_should
   end
 
   def test_have_child_should_not_failure_message
-    m = Accessibility::HasChildMatcher.new(:window, {})
     e = app.window
+    m = have_child(:window)
 
-    m.does_not_match? app
-    assert_equal "Expected #{app.inspect} to NOT have child #{e.inspect}",
-      m.failure_message_for_should_not
+    refute m.does_not_match?(app)
+    assert_equal "Expected #{app.inspect} to NOT have child #{e.inspect}", m.failure_message_for_should_not
   end
 
   def test_have_child_description
-    m = Accessibility::HasChildMatcher.new(:window,{}) {}
+    m = have_child(:window) { }
     assert_equal 'should have a child that matches Window[✔]', m.description
   end
 
+
+
   def test_have_descendent_should_failure_message
-    m = Accessibility::HasDescendentMatcher.new(:button, {}) {}
-    q = Accessibility::Qualifier.new(:Button,{}) {}
+    m = have_descendent(:button)
     e = app.main_window.slider
 
-    m.matches? e
-    assert_equal "Expected #{e.inspect} to have descendent Button[✔]",
-      m.failure_message_for_should
+    refute m.matches?(e)
+    assert_equal "Expected #{e.inspect} to have descendent Button", m.failure_message_for_should
   end
 
   def test_have_descendent_should_not_failure_message
-    m = Accessibility::HasDescendentMatcher.new(:window,{})
+    m = have_descendant(:window)
     e = app.window
 
-    m.does_not_match? app
-    assert_equal "Expected #{app.inspect} to NOT have descendent #{e.inspect}",
-      m.failure_message_for_should_not
+    refute m.does_not_match?(app)
+    assert_equal "Expected #{app.inspect} to NOT have descendent #{e.inspect}", m.failure_message_for_should_not
   end
 
   def test_have_descendent_description
-    m = Accessibility::HasDescendentMatcher.new(:button,{})
+    m = have_descendent(:button)
     assert_equal 'should have a descendent matching Button', m.description
+  end
+
+
+
+  def test_shortly_have_child_should_failure_message
+    m = shortly_have_child(:button, timeout: 0) { }
+    e = app.main_window.slider
+
+    refute m.matches?(e)
+    assert_equal "Expected #{e.inspect} to have child Button[✔] before a timeout occurred",
+      m.failure_message_for_should
+  end
+
+  def test_shortly_have_child_should_not_failure_message
+    m = shortly_have_child(:window)
+    e = app
+
+    refute m.does_not_match?(e)
+    assert_equal "Expected #{e.inspect} to NOT have child #{e.window.inspect} before a timeout occurred",
+      m.failure_message_for_should_not
+  end
+
+  def test_shortly_have_child_description
+    m = shortly_have_child(:button)
+    assert_equal 'should have a child that matches Button before a timeout occurs', m.description
+  end
+
+
+
+  def test_shortly_have_descendent_should_failure_message
+    m = shortly_have_descendent(:button, timeout: 0)
+    e = app.main_window.slider
+
+    refute m.matches?(e)
+    assert_equal "Expected #{e.inspect} to have descendent Button before a timeout occurred",
+      m.failure_message_for_should
+  end
+
+  def test_shortly_have_descendent_should_not_failure_message
+    m = shortly_have_descendent(:window)
+    e = app
+
+    refute m.does_not_match?(e)
+    assert_equal "Expected #{e.inspect} to NOT have descendent #{e.window.inspect} before a timeout occurred",
+      m.failure_message_for_should_not
+  end
+
+  def test_shortly_have_descendent_description
+    m = shortly_have_descendent(:button)
+    assert_equal 'should have a descendent matching Button before a timeout occurs', m.description
   end
 
 end
