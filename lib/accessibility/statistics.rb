b59f79d69c29f1303dcb73ea20c8ff2000782d68
Mark Rada <mrada@marketcircle.com>
2012-08-28 21:20:40 -0400

    Add Accessibility::Statistics for stat tracking Core

---
+++ lib/accessibility/statistics.rb
@@ -0,0 +1,56 @@
+require 'accessibility/version'
+
+class Accessibility::Statistics
+
+  def initialize
+    @stats = Hash.new do |h,k| h[k] = 0 end
+    @q     = Dispatch::Queue.new "com.marketcircle.axelements.stats"
+  end
+
+  def increment key
+    @q.async do
+      @stats[key] += 1
+    end
+  end
+
+  def to_s
+    @q.sync do # must be synchronized
+      set_max_length
+      @out = output_header << output_body << "\n"
+    end
+    @out
+  end
+
+
+  private
+
+  def set_max_length
+    @max_key_len = @stats.keys.map(&:length).max
+    @max_val_len = @stats.values.max.to_s.length
+  end
+
+  def dot key, val
+    length  = 4
+    length += @max_key_len - key.length
+    length += @max_val_len - val.to_s.length
+    "." * length
+  end
+
+  def output_header
+    <<-EOS
+#################
+# AX Statistics #
+#################
+    EOS
+  end
+
+  def output_body
+    @stats.collect do |key, val|
+      "#{key}#{dot(key,val)}#{val}"
+    end.join("\n")
+  end
+
+end
+
+# @return [Accessibility::Statistics]
+STATS = Accessibility::Statistics.new
