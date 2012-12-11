550a594fcd53646aca4524328dcd5a1c23381da6
Mark Rada <mrada@marketcircle.com>
2012-12-11 03:02:15 -0500

    Only set MOUNTAIN_LION_APPKIT_VERSION if it is falsey

--- lib/ax_elements/core_graphics_workaround.rb
+++ lib/ax_elements/core_graphics_workaround.rb
@@ -1,6 +1,6 @@
 framework 'Cocoa'
 
-MOUNTAIN_LION_APPKIT_VERSION = 1187
+MOUNTAIN_LION_APPKIT_VERSION ||= 1187
 if NSAppKitVersionNumber >= MOUNTAIN_LION_APPKIT_VERSION
     framework '/System/Library/Frameworks/CoreGraphics.framework'
 end
