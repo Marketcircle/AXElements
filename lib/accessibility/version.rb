d73f9fbc8327098b8b24e367023802ed3f2e7051
Mark Rada <mrada@marketcircle.com>
2012-12-12 16:50:05 -0500

    Bump version for release

--- lib/accessibility/version.rb
+++ lib/accessibility/version.rb
@@ -4,7 +4,7 @@
 # The main AXElements namespace.
 module Accessibility
   # @return [String]
-  VERSION   = '1.0.0.alpha10'
+  VERSION   = '1.0.0.alpha11'
 
   # @return [String]
   CODE_NAME = 'ルナトーン'
