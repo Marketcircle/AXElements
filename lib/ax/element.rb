e1275e7067b92b590434efea61bc3e0416dd1fcc
Mark Rada <mrada@marketcircle.com>
2012-12-26 01:47:44 -0500

    Define AX::Element#respond_to? differently on MRI
    Since MRI's #respond_to? only takes one argument
    but MacRuby's takes two!

--- lib/ax/element.rb
+++ lib/ax/element.rb
@@ -457,11 +457,17 @@ class AX::Element
     @ref.invalid?
   end
 
-  ##
-  # Like {#respond_to?}, this is overriden to include attribute methods.
-  # Though, it does include dynamic predicate methods at the moment.
-  def methods include_super = true, include_objc_super = false
-    super.concat(attributes).concat(parameterized_attributes)
+  if on_macruby?
+    ##
+    # Like {#respond_to?}, this is overriden to include attribute methods.
+    # Though, it does include dynamic predicate methods at the moment.
+    def methods include_super = true, include_objc_super = false
+      super.concat(attributes).concat(parameterized_attributes)
+    end
+  else
+    def methods include_super = true
+      super.concat(attributes).concat(parameterized_attributes)
+    end
   end
 
   ##
