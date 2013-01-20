8e1d3a9cbd32c454315f5026772e62d75d39ac5b
Mark Rada <mrada@marketcircle.com>
2013-01-20 18:52:06 -0500

    Add ENGINE information to the version

--- lib/accessibility/version.rb
+++ lib/accessibility/version.rb
@@ -8,4 +8,24 @@ module Accessibility
 
   # @return [String]
   CODE_NAME = 'ルナトーン'
+
+  # @return [String]
+  ENGINE = case RUBY_ENGINE
+           when 'macruby' then 'サンダース'
+           when 'ruby'    then 'ブースター'
+           when 'rbx'     then 'ブラッキー' # for when rbx has good cext support
+           else 'シャワーズ' # vapor(ware)eon
+           end
+
+  ##
+  # The complete version string for AXElements
+  #
+  # This differs from {Accessibility::VERSION} in that it also
+  # includes `RUBY_ENGINE` information.
+  #
+  # @return [String]
+  def self.version
+    "#{VERSION}-#{ENGINE}"
+  end
+
 end
